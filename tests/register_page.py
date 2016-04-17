f17a26c4dcc27de6077a3cb570fe71cceb480a0d
alrogovsky <alex@rogovsky.ru>
2016-04-17 19:15:56 +0300

    halfway

--- tests/register_page.py
+++ tests/register_page.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from page import Page
 from component import Component
+from selenium.webdriver.support.ui import Select
 
 
 class RegisterPage(Page):
-    PATH = u'/signup/'
+    PATH = u'/signup/?lang=ru_RU'
 
     def get_form(self):
         return RegisterForm(self.driver)
@@ -17,6 +18,21 @@ class RegisterForm(Component):
     FIRST_NAME_INPUT = u"//*[contains(@class, 'qc-firstname-row')]/span[contains(@class, 'sig2')]/input"
     FIRST_NAME_NOTIF = u"//*[contains(@class, 'qc-firstname-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'example')]"
     FIRST_NAME_ERROR = u"//*[contains(@class, 'qc-firstname-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'error')]"
+    LAST_NAME_INPUT = u"//*[contains(@class, 'qc-lastname-row')]/span[contains(@class, 'sig2')]/input"
+    LAST_NAME_NOTIF = u"//*[contains(@class, 'qc-lastname-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'example')]"
+    LAST_NAME_ERROR = u"//*[contains(@class, 'qc-lastname-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'error')]"
+
+    BIRTHDATE_ERROR = u"//*[contains(@class, 'qc-birthday-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'error')]"
+    BIRTHDATE_SUCCESS = u"//*[contains(@class, 'qc-birthday-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'success')]"
+
+    CITY_INPUT = u"//*[contains(@class, 'qc-city-row')]/span[contains(@class, 'sig2')]/input"
+    CITY_HELPER = u"//*[contains(@class, 'qc-city-row')]/span/*[contains(@class, 'geo_popup')]/span"
+    CITY_ERROR = u"//*[contains(@class, 'qc-city-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'error')]"
+    CITY_SUCCESS = u"//*[contains(@class, 'qc-city-row')]/span[contains(@class, 'sig3')]/span[contains(@class, 'success')]"
+
+    DAY_SELECTOR = u"//*[contains(@class, 'qc-select-day')]"
+    MONTH_SELECTOR = u"//*[contains(@class, 'qc-select-month')]"
+    YEAR_SELECTOR = u"//*[contains(@class, 'qc-select-year')]"
 
     def get_first_name_notif(self):
         return self.driver.find_element_by_xpath(self.FIRST_NAME_NOTIF)
@@ -27,5 +43,49 @@ class RegisterForm(Component):
     def set_first_name(self, name):
         self.driver.find_element_by_xpath(self.FIRST_NAME_INPUT).send_keys(name)
 
+    def get_last_name_notif(self):
+        return self.driver.find_element_by_xpath(self.LAST_NAME_NOTIF)
+
+    def get_last_name_error(self):
+        return self.driver.find_element_by_xpath(self.LAST_NAME_ERROR)
+
+    def set_last_name(self, name):
+        self.driver.find_element_by_xpath(self.LAST_NAME_INPUT).send_keys(name)
+
     def unfocus(self):
         self.driver.find_element_by_xpath(self.TITLE).click()
+
+    def select_day(self, day):
+        day = str(day)
+        el = Select(self.driver.find_element_by_xpath(self.DAY_SELECTOR))
+        el.select_by_visible_text(day)
+
+    def select_month(self, month):
+        el = Select(self.driver.find_element_by_xpath(self.MONTH_SELECTOR))
+        el.select_by_visible_text(month)
+
+    def select_year(self, year):
+        year = str(year)
+        el = Select(self.driver.find_element_by_xpath(self.YEAR_SELECTOR))
+        el.select_by_visible_text(year)
+
+    def get_birthdate_success_el(self):
+        return self.driver.find_element_by_xpath(self.BIRTHDATE_SUCCESS)
+
+    def get_birthdate_error_el(self):
+        return self.driver.find_element_by_xpath(self.BIRTHDATE_ERROR)
+
+    def get_city_input(self):
+        return self.driver.find_element_by_xpath(self.CITY_INPUT)
+
+    def set_city(self, city):
+        self.driver.find_element_by_xpath(self.CITY_INPUT).send_keys(city)
+
+    def get_city_error_el(self):
+        return self.driver.find_element_by_xpath(self.CITY_ERROR)
+
+    def get_city_helper_el(self):
+        return self.driver.find_element_by_xpath(self.CITY_HELPER)
+
+    def get_city_success_el(self):
+        return self.driver.find_element_by_xpath(self.CITY_SUCCESS)
--- tests/registration_test.py
+++ tests/registration_test.py
@@ -16,7 +16,8 @@ class RegistrationTest(unittest.TestCase):
 
         self.register_page = RegisterPage(self.driver)
 
-    def test_firstname_element(self):
+    @unittest.skip('ok')
+    def test_first_name_element(self):
         self.register_page.open()
         form = self.register_page.get_form()
 
@@ -30,6 +31,88 @@ class RegistrationTest(unittest.TestCase):
 
         error = form.get_first_name_error()
         self.assertTrue(error.is_displayed())
+
+    @unittest.skip('ok')
+    def test_last_name_element(self):
+        self.register_page.open()
+        form = self.register_page.get_form()
+
+        self.assertTrue(form.check_exists_by_xpath(form.LAST_NAME_INPUT))
+        self.driver.find_element_by_xpath(form.LAST_NAME_INPUT).click()
+
+        notification = form.get_last_name_notif()
+        self.assertTrue(notification.is_displayed())
+
+        form.unfocus()
+
+        error = form.get_last_name_error()
+        self.assertTrue(error.is_displayed())
+
+    @unittest.skip('ok')
+    def test_birth_date_right_date(self):
+        self.register_page.open()
+        form = self.register_page.get_form()
+
+        form.select_day('11')
+        form.select_month('March')
+        form.select_year('1999')
+        form.unfocus()
+
+        birthdate_success = form.get_birthdate_success_el()
+        self.assertTrue(birthdate_success.is_displayed())
+
+    @unittest.skip('ok')
+    def test_birth_date_wrong_date(self):
+        self.register_page.open()
+        form = self.register_page.get_form()
+
+        form.select_day('30')
+        form.select_month('February')
+        form.select_year('1999')
+        form.unfocus()
+
+        birthdate_success = form.get_birthdate_success_el()
+        birthdate_error = form.get_birthdate_error_el()
+
+        self.assertFalse(birthdate_success.is_displayed())
+        self.assertTrue(birthdate_error.is_displayed())
+
+    @unittest.skip('ok')
+    def test_birth_date_required_field(self):
+        self.register_page.open()
+        form = self.register_page.get_form()
+
+        form.select_day('30')
+        form.unfocus()
+
+        birthdate_error = form.get_birthdate_error_el()
+        self.assertTrue(birthdate_error.is_displayed())
+
+    @unittest.skip('ok')
+    def test_city_only_from_list(self):
+        self.register_page.open()
+        form = self.register_page.get_form()
+
+        form.set_city(u'Гейсити')
+        form.unfocus()
+
+        city_error_el = form.get_city_error_el()
+        self.assertTrue(city_error_el.is_displayed())
+
+    def test_city_autocompletion(self):
+        self.register_page.open()
+        form = self.register_page.get_form()
+
+        form.set_city(u'Москва')
+        self.assertTrue(form.check_exists_by_xpath(form.CITY_HELPER))
+
+        helper = form.get_city_helper_el()
+        city = helper.find_element_by_tag_name('div')
+        city.click()
+
+        city_success_el = form.get_city_success_el()
+        self.assertTrue(city_success_el.is_displayed())
+
 
 
     def tearDown(self):
