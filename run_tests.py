021a93171dfd48d3cff59931b827dad0e3cd3d82
Sasha Konyshev <sasha.konyshev1003@gmail.com>
2016-04-17 07:23:55 -0700

    Add tests to calendar table

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python2
 
+import sys
 import unittest
+# from tests.example_test import ExampleTest
+from tests.test import AuthTest, MonthToolbarTest, HeaderTest, CalendarTableTest, SidebarTest
+
 
 if __name__ == '__main__':
-    pass
+    suite = unittest.TestSuite((
+        unittest.makeSuite(AuthTest),
+        unittest.makeSuite(MonthToolbarTest),
+        unittest.makeSuite(HeaderTest),
+        unittest.makeSuite(CalendarTableTest),
+        unittest.makeSuite(SidebarTest),
+    ))
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
