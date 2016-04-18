6890d1e58cbc788a2aa4681e97c77379dd69d715
evgeny-ivanov <Saboteuur@yandex.ru>
2016-04-18 20:19:51 +0300

    70 %

--- pages/main_page_pages.py
+++ pages/main_page_pages.py
@@ -9,12 +9,15 @@ class HeadMailPage:
 
     def login(self, login, password):
         self.driver.find_element_by_id("PH_authLink").click()
+        self.driver.switch_to_frame(self.driver.find_elements_by_css_selector(".ag-popup__frame__layout__iframe")[0])
 
-        inputs = self.driver.find_elements_by_class_name("x-ph__form__input__text")
-        inputs[0].send_keys(login)
-        inputs[1].send_keys(password)
+        input_email = self.driver.find_elements_by_css_selector(".b-email__name > input:nth-child(1)")[0]
+        input_email.send_keys(login)
 
-        self.driver.find_elements_by_class_name("x-ph__button__input")[0].click()
+        input_password = self.driver.find_elements_by_css_selector("div.b-input-btn__cell:nth-child(1) > :nth-child(1)")[0]
+        input_password.send_keys(password)
+
+        self.driver.find_elements_by_css_selector("div.b-input-btn__cell:nth-child(2) > :nth-child(1)")[0].click()
 
     def logout(self):
         button_logout = self.driver.find_element_by_id("PH_logoutLink")
@@ -89,20 +92,45 @@ class PortalMenuSubmenuPage:
 class AdvertisingUnitPage:
     def __init__(self, driver):
         self.driver = driver
-        self.links = ["div.item_small:nth-child(2)","div.item:nth-child(1)","#canvas",
+        self.links = ["div.cols__column_medium_percent-50:nth-child(1) > :nth-child(1)",
+                      "div.cols__column_medium_percent-50:nth-child(2) > :nth-child(1)",
                       "td.yap-layout__item:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(2) > yatag:nth-child(1) > yatag:nth-child(1) > a:nth-child(1)",
                       "td.yap-layout__item:nth-child(2) > yatag:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(2) > yatag:nth-child(1) > yatag:nth-child(1) > a:nth-child(1)",
                       "td.yap-layout__item:nth-child(3) > yatag:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(2) > yatag:nth-child(1) > yatag:nth-child(1) > a:nth-child(1)"]
 
+        self.advertising_close_button = ".yap-layout_type_wide > yatag:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(3) > yatag:nth-child(1) > yatag:nth-child(1)"
+        self.advertising = ".yap-layout_type_wide > yatag:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > yatag:nth-child(1) > yatag:nth-child(1)"
+
     def click_link(self, selector):
         link = self.driver.find_elements_by_css_selector(selector)[0]
         link.click()
+
+    def hide_advertising(self):
+        advertising = self.driver.find_elements_by_css_selector(self.advertising_close_button)[0]
+        advertising.click()
+
+        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.driver.find_elements_by_css_selector("label.radiobox__radio:nth-child(2)")[0].click()
+        self.driver.find_elements_by_css_selector(".button")[0].click()
+
+        self.driver.switch_to_window(self.driver.window_handles[0])
+
+    def get_baner_text(self):
+        element = self.driver.find_elements_by_css_selector(".yap-layout__item_abused > yatag:nth-child(1) > yatag:nth-child(2) > yatag:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)")[0]
+        return element.text
+
+    def move_to_advertising(self):
+        advertising = self.driver.find_elements_by_css_selector(self.advertising)[0]
+        action = webdriver.ActionChains(self.driver)
+        action.move_to_element(advertising).perform()
+
 
 class BlockHoroPage:
     def __init__(self, driver):
         self.driver = driver
         self.date_block = ".p-prediction__date__text"
         self.title = "h1.hdr__inner"
+        self.date_birth = "span.link:nth-child(3) > span:nth-child(1)"
 
     def get_date(self):
         element = self.driver.find_elements_by_css_selector(self.date_block)[0]
@@ -112,8 +140,88 @@ class BlockHoroPage:
         element = self.driver.find_elements_by_css_selector(self.title)[0]
         return element.text
 
+    def get_date_birth(self):
+        element = self.driver.find_elements_by_css_selector(self.date_birth)[0]
+        return element.text
+
     def click_button_choice_date(self, index):
         selector = "a.filter__item_white:nth-child(" + str(index) + ")"
         link = self.driver.find_elements_by_css_selector(selector)[0]
         link.click()
 
+
+
+class ZodiacSignPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.day = "div.dropdown:nth-child(1) > div:nth-child(1)"
+        self.month = "div.dropdown:nth-child(2) > div:nth-child(1)"
+        self.year = "div.dropdown:nth-child(3) > div:nth-child(1)"
+
+    def select_date(self, day, month, year):
+        dayInput = self.driver.find_elements_by_css_selector(self.day)[0]
+        monthInput = self.driver.find_elements_by_css_selector(self.month)[0]
+        yearInput = self.driver.find_elements_by_css_selector(self.year)[0]
+
+        dayInput.click()
+        selector = "div.dropdown__item:nth-child(" + str(day) + ")"
+        self.driver.find_elements_by_css_selector(selector)[0].click()
+
+        monthInput.click()
+        selector = "div.dropdown:nth-child(2) > div:nth-child(2) > div:nth-child(1) > [data-optidx='" + str(month - 1) + "']"
+        self.driver.find_elements_by_css_selector(selector)[0].click()
+
+
+    def click_all_horo(self):
+        self.driver.find_elements_by_css_selector(".button_top")[0].click()
+
+    def move_to_image_sing_zodiac(self):
+        pass
+
+class SearchDreamPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.searchInput = ".p-formitem_subwidth > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
+        self.searchButton = "button.margin_left_10"
+        self.alphabet = lambda index: ".wrapper_nat > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(" + str(index) + ") > span:nth-child(1)"
+
+    def search_by_text(self, dream):
+        search_input = self.driver.find_elements_by_css_selector(self.searchInput)[0]
+        search_input.send_keys(dream)
+
+        button = self.driver.find_elements_by_css_selector(self.searchButton)[0]
+        button.click()
+
+    def search_by_alphabet(self, index):
+        alphabet = self.driver.find_elements_by_css_selector(self.alphabet(index))[0]
+        alphabet.click()
+
+
+class LunisolarForecastPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.date = ".p-content-item__number__inner"
+
+    def get_date(self):
+        element = self.driver.find_elements_by_css_selector(".p-content-item__number__inner")[0]
+        return element.text
+
+
+class SubscriptionUnitPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.vk_link_selector = "a.icon:nth-child(2)"
+        self.facebook_link_selector = "a.icon:nth-child(3)"
+        self.ok_link_selector = "a.icon:nth-child(4)"
+
+    def go_group_vk(self):
+        link = self.driver.find_elements_by_css_selector(self.vk_link_selector)[0]
+        link.click()
+
+    def go_group_facebook(self):
+        link = self.driver.find_elements_by_css_selector(self.facebook_link_selector)[0]
+        link.click()
+
+    def go_group_ok(self):
+        link = self.driver.find_elements_by_css_selector(self.ok_link_selector)[0]
+        link.click()
--- test_cases/main_page_test_case.py
+++ test_cases/main_page_test_case.py
@@ -11,16 +11,96 @@ from pages.main_page_pages import PortalMenuToolbarPage
 from pages.main_page_pages import PortalMenuSubmenuPage
 from pages.main_page_pages import AdvertisingUnitPage
 from pages.main_page_pages import BlockHoroPage
+from pages.main_page_pages import ZodiacSignPage
+from pages.main_page_pages import SearchDreamPage
+from pages.main_page_pages import LunisolarForecastPage
+from pages.main_page_pages import SubscriptionUnitPage
 
 #тесты иногда падают когда что то на странице не догрузилось(чертов ajax)
 #бывает падают тесты из за защиты мэйла на подбор пароля
+
+#авторизация больше не работает - поменялся DOM + почему то элементы ввода больше заселектить нельзя
 
 def tune_driver():
     # self.driver = webdriver.Chrome('./chromedriver')
     driver = webdriver.Firefox()
     driver.get("https://horo.mail.ru/")
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     return driver
+
+
+def _get_zodiac_sign_by_date(month, day):
+    if (month == 3 and 21 <= day <= 31) or (month == 4 and 1 <= day <= 20):
+        return u"Овен"
+    if (month == 4 and 21 <= day <= 30) or (month == 5 and 1 <= day <= 20):
+        return u"Телец"
+    if (month == 5 and 21 <= day <= 31) or (month == 6 and 1 <= day <= 20):
+        return u"Близнецы"
+    if (month == 6 and 21 <= day <= 30) or (month == 7 and 1 <= day <= 22):
+        return u"Рак"
+    if (month == 7 and 23 <= day <= 31) or (month == 8 and  1 <= day <= 22):
+        return u"Лев"
+    if (month == 8  and 23 <= day <= 31) or (month == 9 and  1 <= day <= 23):
+        return u"Дева"
+    if (month == 9 and 24 <= day <= 30) or (month == 10 and  1 <= day <= 23):
+        return u"Весы"
+    if (month == 10 and 24 <= day <= 31) or (month == 11 and  1 <= day <= 21):
+        return u"Скорпион"
+    if (month == 11 and 22 <= day <= 30) or (month == 12 and 1 <= day <= 21):
+        return u"Стрелец"
+    if (month == 12 and 22 <= day <= 31) or (month == 1 and  1 <= day <= 19):
+        return u"Козерог"
+    if (month == 1 and 22 <= day <= 30) or (month == 2 and 1 <= day <= 18):
+        return u"Водолей"
+    if (month == 2 and 19 <= day <= 29) or (month == 3 and 1 <= day <= 20):
+        return u"Рыбы"
+
+_months = (
+    u"января",
+    u"февраля",
+    u"марта",
+    u"апреля",
+    u"мая",
+    u"июня",
+    u"июля",
+    u"августа",
+    u"сентября",
+    u"октября",
+    u"ноября",
+    u"декабря",
+)
+
+_alphabet = (
+    "a",
+    "b",
+    "v",
+    "g",
+    "d",
+    "e",
+    "zh",
+    "z",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "f",
+    "h",
+    "ts",
+    "ch",
+    "sh",
+    "shh",
+    "je",
+    "ju",
+    "ja",
+)
 
 class HeadMailPageTestCase(unittest.TestCase):
     def setUp(self):
@@ -28,7 +108,7 @@ class HeadMailPageTestCase(unittest.TestCase):
         self.page = HeadMailPage(self.driver)
 
     def tearDown(self):
-        self.driver.quit()
+        pass #self.driver.quit()
 
     def _test_links_head(self):
         urls = ["https://mail.ru","https://e.mail.ru","https://my.mail.ru",
@@ -46,13 +126,15 @@ class HeadMailPageTestCase(unittest.TestCase):
 
     def _test_login_incorrect(self):
         self.page.login(login="ErrorError", password="ErrorError")
-
+        #только вчера эта функциональность работала совсем по другому
         text_login_incorrect = u"Неверное имя пользователя или пароль. Проверьте правильность введенных данных."
         text_color_login_incorrect = "rgba(234, 0, 0, 1)"
 
-        if "https://e.mail.ru" not in self.driver.current_url:#при частом вводе некоректных данных кидает на другую страницу
-            self.assertEquals(self.page.get_text_login_incorrect(),text_login_incorrect)
-            self.assertEquals(self.page.get_color_text_login_incorrect(),text_color_login_incorrect)
+        #if "https://e.mail.ru" not in self.driver.current_url:#при частом вводе некоректных данных кидает на другую страницу
+        #    self.assertEquals(self.page.get_text_login_incorrect(),text_login_incorrect)
+        #    self.assertEquals(self.page.get_color_text_login_incorrect(),text_color_login_incorrect)
+
+        self.assertIn("https://account.mail.ru/user/login", self.driver.current_url)
 
     def _test_login_correct(self):
         login = "myLogin@list.ru"
@@ -73,11 +155,11 @@ class PortalMenuToolbarPageTestCase(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def _test_click_logo(self):
+    def test_click_logo(self):
         self.page.click_logo()
         self.assertIn("https://lady.mail.ru/", self.driver.current_url)
 
-    def _test_move_to_link(self):
+    def test_move_to_link(self):
         self.assertEquals(self.page.get_background_color_link(), u'transparent')
         self.page.move_to_link()
         time.sleep(10)
@@ -90,7 +172,7 @@ class PortalMenuSubmenuPageTestCase(unittest.TestCase):
         self.driver = tune_driver()
         self.page = PortalMenuSubmenuPage(self.driver)
 
-    def _test_dropdown_move_to_show(self):
+    def test_dropdown_move_to_show(self):
         self.assertFalse(self.page.dropdown_is_open())
         self.page.move_to_link()
         time.sleep(10)
@@ -105,13 +187,21 @@ class AdvertisingUnitPageTestCase(unittest.TestCase):
         self.driver = tune_driver()
         self.page = AdvertisingUnitPage(self.driver)
 
-    def _test_click_all_links(self): #незнаю как получить url новой вкладки
+    def test_click_all_links(self): #незнаю как получить url новой вкладки
         last_url = self.driver.current_url
         for selector in self.page.links:
             self.page.click_link(selector)
-            time.sleep(10)
+            self.driver.switch_to_window(self.driver.window_handles[1])
             self.assertNotEquals(last_url, self.driver.current_url)
-            self.driver.back()
+            self.driver.close()
+            self.driver.switch_to_window(self.driver.window_handles[0])
+
+    def test_hide_advertising(self):
+        self.page.move_to_advertising()
+        self.page.hide_advertising()
+        text = u"Спасибо. Объявление скрыто."
+
+        self.assertEquals(text, self.page.get_baner_text())
 
     def tearDown(self):
         self.driver.quit()
@@ -122,27 +212,13 @@ class BlockHoroTestCase(unittest.TestCase):
         self.driver = tune_driver()
         self.page = BlockHoroPage(self.driver)
 
-    def _test_today_date(self):
+    def test_today_date(self):
         date = datetime.date.today()
-        months = [
-            u"Января",
-            u"Февраля",
-            u"Марта",
-            u"Апреля",
-            u"Мая",
-            u"Июня",
-            u"Июля",
-            u"Августа",
-            u"Сентября",
-            u"Октября",
-            u"Ноября",
-            u"Декабря",
-        ]
         date_site = self.page.get_date().split("\n")
         self.assertEquals(date.day, int(date_site[0]))
-        self.assertEquals(months[date.month - 1].upper(), date_site[1].upper())
+        self.assertEquals(_months[date.month - 1].upper(), date_site[1].upper())
 
-    def _test_horo_for_all(self):
+    def test_horo_for_all(self):
         title = u"Гороскоп для всех знаков на сегодня"
         self.assertEquals(title, self.page.get_horo_title())
 
@@ -154,9 +230,110 @@ class BlockHoroTestCase(unittest.TestCase):
             full_title = title + date[i - 1]
             self.assertEquals(full_title, self.page.get_horo_title())
 
+    def test_zodiac_sign(self):
+        head_mail_page = HeadMailPage(self.driver)
+        head_mail_page.login("MyLogin@mail.ru", "helloworld")
+
+        date_birth = self.page.get_date_birth().split(" ")
+        day = int(date_birth[0])
+        month = _months.index(date_birth[1]) + 1
+        print day, month
+
+        zodiac_sign = self.page.get_horo_title().split(" ")[-1]
+        self.assertEquals(zodiac_sign,_get_zodiac_sign_by_date(month,day))
+
+        head_mail_page.logout()
+
+    def tearDown(self):
+        self.driver.quit()
+
+class ZodiacSignTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = tune_driver()
+        self.page = ZodiacSignPage(self.driver)
+
+    def test_select_date(self):
+        day = 2
+        month = 4
+        year = 1999
+        self.page.select_date(day,month,year)
+        self.page.click_all_horo()
+
+        block_horo_page = BlockHoroPage(self.driver)
+        zodiac_sing = block_horo_page.get_horo_title().split(" ")[-1]
+
+        self.assertEquals(zodiac_sing, _get_zodiac_sign_by_date(month,day))
+
+    def tearDown(self):
+        self.driver.quit()
+
+class SearchDreamTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = tune_driver()
+        self.page = SearchDreamPage(self.driver)
+
+    def test_search_dream(self):
+        dream = "testDream"
+        self.page.search_by_text(dream)
+
+        self.assertIn(dream, self.driver.current_url)
+
+    def test_search_alphabet(self):
+        for character_index in range(29):
+            character = _alphabet[character_index]
+            self.page.search_by_alphabet(character_index + 1)
+            self.assertIn("https://horo.mail.ru/sonnik/" + character + "/", self.driver.current_url)
+            self.driver.back()
+
     def tearDown(self):
         self.driver.quit()
 
 
+class LunisolarForecastTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = tune_driver()
+        self.page = LunisolarForecastPage(self.driver)
+
+    def test_check_date(self):
+        date = datetime.date.today()
+        date_site = self.page.get_date().split("\n")
+        self.assertEquals(date.day, int(date_site[0]))
+        self.assertEquals(_months[date.month - 1].upper(), date_site[1].upper())
+
+    def tearDown(self):
+        self.driver.quit()
+
+class SubscriptionUnitTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = tune_driver()
+        self.page = SubscriptionUnitPage(self.driver)
+        self.url_vk = "https://vk.com/ladymailru"
+        self.url_facebook = "https://www.facebook.com/lady.mail.ru?ref=bookmarks"
+        self.url_ok = "http://ok.ru/ladymailru"
+
+    def _test_go_group_vk(self):
+        self.page.go_group_vk()
+        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.assertEqual(self.driver.current_url, self.url_vk)
+        self.driver.close()
+        self.driver.switch_to_window(self.driver.window_handles[0])
+
+    def _test_go_group_facebook(self):
+        self.page.go_group_facebook()
+        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.assertEqual(self.driver.current_url, self.url_facebook)
+        self.driver.close()
+        self.driver.switch_to_window(self.driver.window_handles[0])
+
+    def _test_go_group_ok(self):
+        self.page.go_group_ok()
+        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.assertEqual(self.driver.current_url, self.url_ok)
+        self.driver.close()
+        self.driver.switch_to_window(self.driver.window_handles[0])
+
+    def tearDown(self):
+        self.driver.quit()
 
 
+#print x.decode('unicode-escape')
