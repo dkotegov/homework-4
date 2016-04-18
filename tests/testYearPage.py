177ab849a38d681c5cef32eb1a97c1357acf456a
clockworkrage <kamvalentin@gmail.com>
2016-04-18 23:53:07 +0300

    test year page

---
+++ tests/testYearPage.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+import os
+
+import unittest
+import urlparse
+import time
+
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from datetime import date
+
+class Page(object):
+    BASE_URL = 'https://calendar.mail.ru/'
+    PATH = ''
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        url = urlparse.urljoin(self.BASE_URL, self.PATH)
+        self.driver.get(url)
+        self.driver.maximize_window()
+
+class AuthPage(Page):
+    PATH = ''
+
+    @property
+    def form(self):
+        return AuthForm(self.driver)
+
+
+
+class Component(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+class AuthForm(Component):
+    LOGIN = '//input[@name="Login"]'
+    PASSWORD = '//input[@name="Password"]'
+    SUBMIT = '//button[text()="Войти"]'
+    YEAR_BUTTON = '//span[@name="year"]'
+
+    def set_login(self, login):
+        self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
+
+    def set_password(self, pwd):
+        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(pwd)
+
+    def submit(self):
+        self.driver.find_element_by_xpath(self.SUBMIT).click()
+
+    def year(self):
+        self.driver.find_element_by_xpath(self.YEAR_BUTTON).click()
+
+class YearNav(Page):
+    @property
+    def nav(self):
+        return Nav(self.driver)
+
+class Nav(Component):
+    YEAR_CAPTION = '//div[@class="year-caption"]'
+    PREV_BUTTON = '//a[@class="button button_color_dark grid-prev"]'
+    NEXT_BUTTON = '//a[@class="button button_color_dark grid-next"]'
+
+    def prev_period(self):
+        self.driver.find_element_by_xpath(self.PREV_BUTTON).click()
+
+    def next_period(self):
+        self.driver.find_element_by_xpath(self.NEXT_BUTTON).click()
+
+    def get_year(self):
+        return WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.YEAR_CAPTION).text
+        )
+
+class Calendar_table(Page):
+    @property
+    def table(self):
+        return year_table(self.driver)
+
+class year_table(Component):
+    DAY_BUTTON = '//a[@data-day="12"]'
+    WEEK_TABLE = '//div[@class="weekTmp"]'
+
+    def select_day(self):
+        self.driver.find_element_by_xpath(self.DAY_BUTTON).click()
+    def test_week(self):
+        try:
+            self.driver.find_element_by_xpath(self.WEEK_TABLE)
+            return True
+        except NoSuchElementException:
+            return False
+
+class Preferences(Page):
+    @property
+    def pref(self):
+        return side_bar(self.driver)
+
+class side_bar(Component):
+    OPEN_BUTTON = '//div[@class="preferences__button button button_color_dark"]'
+    PREF_OPEN = '//div[@class="preferences preferences_open"]'
+
+    def test_click(self):
+        self.driver.find_element_by_xpath(self.OPEN_BUTTON).click()
+
+    def test_open(self):
+        try:
+            self.driver.find_element_by_xpath(self.PREF_OPEN)
+            return True
+        except NoSuchElementException:
+            return False
+
+
+class Tests(unittest.TestCase):
+    USERNAME = u'Test1232323'
+    PASSWORD = u'Qq654321'
+
+    def setUp(self):
+
+        self.driver = webdriver.Firefox()
+
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+
+        auth_form = auth_page.form
+        auth_form.set_login(self.USERNAME)
+        auth_form.set_password(self.PASSWORD)
+        auth_form.submit()
+        auth_form.year()
+
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_period_navigation(self):
+        current_year = date.today().year
+        year_nav = YearNav(self.driver)
+        navigation = year_nav.nav
+        self.assertEqual(str(current_year), navigation.get_year())
+
+        navigation.prev_period()
+        self.assertEqual(str(current_year - 1), navigation.get_year())
+
+        navigation.next_period()
+        self.assertEqual(str(current_year), navigation.get_year())
+
+    def test_day(self):
+        calendar_table = Calendar_table(self.driver)
+        table = calendar_table.table
+        table.select_day()
+        self.assertEqual(True,table.test_week())
+
+    def test_pref(self):
+        pref_bar = Preferences(self.driver)
+        bar = pref_bar.pref
+        bar.test_click()
+        self.assertEqual(True, bar.test_open())
+
+        bar.test_click()
+        self.assertEqual(False, bar.test_open())
