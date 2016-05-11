bd02b0a083a96fd1b6bf9b073c3bfcee37c1a95e
kirill-m <kmatveev15@gmail.com>
2016-05-11 15:52:23 +0300

    test updated

--- test_toolbar.py
+++ test_toolbar.py
@@ -5,6 +5,7 @@ from selenium.webdriver import Remote, DesiredCapabilities
 import os
 
 from toolbar_page import Toolbar
+from picture_day import PictureDay, MainPage, OtvetPage
 
 
 class ToolbarTestCase(unittest.TestCase):
@@ -71,3 +72,137 @@ class ToolbarTestCase(unittest.TestCase):
         search.pressEnterKey()
         self.assertEqual(search.QUERY_URL, search.getURL())
         self.assertEqual(search.getStatus(search.QUERY_URL), self.OK)
+
+
+    def test_mail_login_wrong_login(self):
+
+        LOGIN = "seleniumofff_lol"
+        PASSWORD = "123456qwerty"
+        
+        pictureday = MainPage(self.driver)
+        pictureday.open()
+
+        mail_login = pictureday.feed_back
+
+        mail_login.set_login(LOGIN)
+        mail_login.set_password(PASSWORD)
+        mail_login.submit()
+        self.assertEqual(mail_login.WRONG_LOGIN_URL, mail_login.get_url())
+
+    def test_mail_login_wrong_password(self):
+
+        LOGIN = "seleniumov"
+        PASSWORD = "123456qwerty1234"
+        
+        pictureday = MainPage(self.driver)
+        pictureday.open()
+
+        mail_login = pictureday.feed_back
+
+        mail_login.set_login(LOGIN)
+        mail_login.set_password(PASSWORD)
+        mail_login.submit()
+        self.assertEqual(mail_login.WRONG_PSWD_URL, mail_login.get_url())     
+
+   	def test_mail_login_empty_pswd(self):
+
+        LOGIN = "seleniumov"
+        PASSWORD = ""
+
+        pictureday = MainPage(self.driver)
+        pictureday.open()
+
+        mail_login = pictureday.feed_back
+
+        mail_login.set_login(LOGIN)        
+        mail_login.set_password(PASSWORD)
+        mail_login.submit()
+        self.assertEqual(mail_login.EMPTY_PSWD_URL, mail_login.get_url())
+
+    def test_ask_good_logout(self):
+
+        URL = "https://otvet.mail.ru/"
+        
+        pictureday = OtvetPage(self.driver)
+        pictureday.open()
+
+        mail_ask = pictureday.mail_ask
+
+        mail_ask.clickAsk()
+
+        mail_ask.iframe_select()
+        mail_ask.set_login("seleniumov")
+        mail_ask.set_password("123456qwerty")
+        mail_ask.click_submit()
+
+        mail_ask.click_logout()
+        self.assertEqual(URL, mail_ask.get_url())
+
+    def test_wrong_body(self):
+
+        URL = "https://otvet.mail.ru/question/"
+        ERROR = u'Просьба более подробно и грамотно сформулировать тему вопроса.'
+
+        QUEST = u'авыравыоалвыоалдвылащвыоавыра'
+
+        
+        pictureday = OtvetPage(self.driver)
+        pictureday.open()
+
+        mail_ask = pictureday.mail_ask
+
+        mail_ask.clickAsk()
+
+        mail_ask.iframe_select()
+        mail_ask.set_login("seleniumov")
+        mail_ask.set_password("123456qwerty")
+        mail_ask.click_submit()
+
+        mail_ask.clickAsk() 
+        self.driver.switch_to_default_content()
+        element = WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located((By.XPATH, "//textarea[@id='ask-text']"))
+        )
+        mail_ask.set_question(QUEST)
+
+        element = WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-orange ask-btn-submit']"))
+        )
+        
+        mail_ask.click_public()
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "popup--content"))
+        )
+        self.assertEqual(ERROR, mail_ask.get_error_text())
+
+    def test_ask_correct(self):
+
+        URL = "https://otvet.mail.ru/question/"
+
+        QUEST = u'Почему в Норвегии хорошо живут?'
+
+        pictureday = OtvetPage(self.driver)
+        pictureday.open()
+
+        mail_ask = pictureday.mail_ask
+
+        mail_ask.clickAsk()
+
+        mail_ask.iframe_select()
+        mail_ask.set_login("seleniumov")
+        mail_ask.set_password("123456qwerty")
+        mail_ask.click_submit()
+
+        mail_ask.clickAsk() 
+
+        self.driver.switch_to_default_content()
+        mail_ask.set_question(QUEST)
+        mail_ask.click_cid()
+        mail_ask.click_subcid()
+        mail_ask.click_public()
+
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "actions"))
+        )
+
+        self.assertIn(URL, self.driver.current_url) 
