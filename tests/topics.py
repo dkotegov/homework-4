ef5df7edb2f20c21c05a796bc1d1f872b17e0db8
IlyaNikk <vic096@yandex.ru>
2017-06-04 23:36:50 +0300

    Change url

--- tests/topics.py
+++ tests/topics.py
@@ -44,8 +44,7 @@ class TopicsTest(unittest.TestCase):
         browser = os.environ.get('BROWSER', 'CHROME')
 
         self.driver = Remote(
-            # command_executor='http://127.0.0.1:4444/wd/hub',
-            command_executor='http://192.168.0.102:5555/wd/hub',
+            command_executor='http://127.0.0.1:4444/wd/hub',
             desired_capabilities=getattr(DesiredCapabilities, browser).copy()
         )
         try:
