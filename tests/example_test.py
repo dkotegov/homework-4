feb76324f9f747ac330062f4337ffcd5e228129a
vladpereskokov <vladislavpereskokov@yandex.ru>
2017-11-11 13:34:16 +0300

    add scripts and change test

--- tests/example_test.py
+++ tests/example_test.py
@@ -147,12 +147,9 @@ class Topic(Component):
 
 
 class ExampleTest(unittest.TestCase):
-    USERNAME = u'Дмитрий Котегов'
-    USEREMAIL = 'kotegov_dima@mail.ru'
+    USERNAME = u'Владислав Перескоков'
+    USEREMAIL = 'vladpereskokov@mail.ru'
     PASSWORD = os.environ['PASSWORD']
-    BLOG = 'Флудилка'
-    TITLE = u'ЗаГоЛоВоК'
-    MAIN_TEXT = u'Текст под катом! Отображается внутри топика!'
 
     def setUp(self):
         browser = os.environ.get('BROWSER', 'CHROME')
@@ -177,27 +174,3 @@ class ExampleTest(unittest.TestCase):
 
         user_name = auth_page.top_menu.get_username()
         self.assertEqual(self.USERNAME, user_name)
-
-        create_page = CreatePage(self.driver)
-        create_page.open()
-
-        create_form = create_page.form
-        create_form.blog_select_open()
-        create_form.blog_select_set_option(self.BLOG)
-        create_form.set_title(self.TITLE)
-        create_form.set_main_text(self.MAIN_TEXT)
-        create_form.set_unpublish()
-        create_form.submit()
-
-        topic_page = TopicPage(self.driver)
-        topic_title = topic_page.topic.get_title()
-        topic_text = topic_page.topic.get_text()
-        self.assertEqual(self.TITLE, topic_title)
-        self.assertEqual(self.MAIN_TEXT, topic_text)
-
-        blog_page = BlogPage(self.driver)
-        blog_page.topic.delete()
-        topic_title = blog_page.topic.get_title()
-        topic_text = blog_page.topic.get_text()
-        self.assertNotEqual(self.TITLE, topic_title)
-        self.assertNotEqual(self.MAIN_TEXT, topic_text)
