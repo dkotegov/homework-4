8b5fe5b83ec9bbc1b8f7fd385da63126c92cf502
vladpereskokov <vladislavpereskokov@yandex.ru>
2017-11-11 14:50:14 +0300

    add pages file

--- run_tests.py
+++ run_tests.py
@@ -2,12 +2,11 @@
 
 import sys
 import unittest
-from vp_check_list.test import ExampleTest
-
+from vp_check_list.test import LoginTest
 
 if __name__ == '__main__':
-    suite = unittest.TestSuite((
-        unittest.makeSuite(ExampleTest),
-    ))
-    result = unittest.TextTestRunner().run(suite)
-    sys.exit(not result.wasSuccessful())
+	suite = unittest.TestSuite((
+		unittest.makeSuite(LoginTest),
+	))
+	result = unittest.TextTestRunner().run(suite)
+	sys.exit(not result.wasSuccessful())
--- vp_check_list/test.py
+++ vp_check_list/test.py
@@ -3,90 +3,36 @@
 import os
 
 import unittest
-import urlparse
 
 from selenium.webdriver import DesiredCapabilities, Remote
-from selenium.webdriver.support.ui import WebDriverWait
+
+from vp_check_list.pages import AuthPage
 
 
-class Page(object):
-    BASE_URL = 'https://www.ok.ru'
-    PATH = ''
+class LoginTest(unittest.TestCase):
+	USERNAME = u'Илья Раков'
+	USER_EMAIL = 'technopark34'
+	PASSWORD = os.environ['OK_PASSWORD']
 
-    def __init__(self, driver):
-        self.driver = driver
+	def setUp(self):
+		browser = os.environ.get('BROWSER', 'CHROME')
 
-    def open(self):
-        url = urlparse.urljoin(self.BASE_URL, self.PATH)
-        self.driver.get(url)
-        self.driver.maximize_window()
+		self.driver = Remote(
+			command_executor='http://127.0.0.1:4444/wd/hub',
+			desired_capabilities=getattr(DesiredCapabilities, browser).copy()
+		)
 
+	def tearDown(self):
+		self.driver.quit()
 
-class AuthPage(Page):
-    PATH = ''
+	def test(self):
+		auth_page = AuthPage(self.driver)
+		auth_page.open()
 
-    @property
-    def form(self):
-        return AuthForm(self.driver)
+		auth_form = auth_page.form
+		auth_form.set_login(self.USER_EMAIL)
+		auth_form.set_password(self.PASSWORD)
+		auth_form.submit()
 
-    @property
-    def top_menu(self):
-        return TopMenu(self.driver)
-
-
-class Component(object):
-    def __init__(self, driver):
-        self.driver = driver
-
-
-class AuthForm(Component):
-    LOGIN = '//input[@id="field_email"]'
-    PASSWORD = '//input[@id="field_password"]'
-    LOGIN_BUTTON = '//input[@class="button-pro __wide"]'
-
-    def set_login(self, login):
-        self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
-
-    def set_password(self, pwd):
-        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(pwd)
-
-    def submit(self):
-        self.driver.find_element_by_xpath(self.LOGIN_BUTTON).click()
-
-
-class TopMenu(Component):
-    USERNAME = '//h1[@class="mctc_name_tx bl"]'
-
-    def get_username(self):
-        return WebDriverWait(self.driver, 30, 0.1).until(
-            lambda d: d.find_element_by_xpath(self.USERNAME).text
-        )  
-
-
-class ExampleTest(unittest.TestCase):
-    USERNAME = u'Илья Раков'
-    USER_EMAIL = 'technopark34'
-    PASSWORD = os.environ['OK_PASSWORD']
-
-    def setUp(self):
-        browser = os.environ.get('BROWSER', 'CHROME')
-
-        self.driver = Remote(
-            command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=getattr(DesiredCapabilities, browser).copy()
-        )
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test(self):
-        auth_page = AuthPage(self.driver)
-        auth_page.open()
-
-        auth_form = auth_page.form
-        auth_form.set_login(self.USER_EMAIL)
-        auth_form.set_password(self.PASSWORD)
-        auth_form.submit()
-
-        user_name = auth_page.top_menu.get_username()
-        self.assertEqual(self.USERNAME, user_name)
+		user_name = auth_page.top_menu.get_username()
+		self.assertEqual(self.USERNAME, user_name)
