34ea0ee1d80cc44dc9abca486d69619e7d65290a
vladpereskokov <vladislavpereskokov@yandex.ru>
2017-11-12 21:34:02 +0300

    change setup and teardown to class

--- vp_check_list/comments_test.py
+++ vp_check_list/comments_test.py
@@ -12,21 +12,23 @@ from vp_check_list.pages.pages import UserPage
 class CommentsTest(unittest.TestCase):
 	TEST_COMMENT = 'Test comment'
 
-	def setUp(self):
+	@classmethod
+	def setUpClass(cls):
 		browser = os.environ.get('BROWSER', 'CHROME')
 
-		self.driver = Remote(
+		cls.driver = Remote(
 			command_executor='http://127.0.0.1:4444/wd/hub',
 			desired_capabilities=getattr(DesiredCapabilities, browser).copy()
 		)
 
-		self.user_page = UserPage(self.driver)
-		self.user_page.login()
+		cls.user_page = UserPage(cls.driver)
+		cls.user_page.login()
 
-		self.post = self.user_page.post
+		cls.post = cls.user_page.post
 
-	def tearDown(self):
-		self.driver.quit()
+	@classmethod
+	def tearDownClass(cls):
+		cls.driver.quit()
 
 	def test_add_comment(self):
 		self.post.add_comment(self.TEST_COMMENT)
