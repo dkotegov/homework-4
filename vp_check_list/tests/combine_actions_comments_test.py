59f138537b160c2f33d930e217699c1ad6254d07
vladpereskokov <vladislavpereskokov@yandex.ru>
2017-11-15 18:13:41 +0300

    add new tet func

--- vp_check_list/tests/combine_actions_comments_test.py
+++ vp_check_list/tests/combine_actions_comments_test.py
@@ -37,3 +37,20 @@ class CombineActionsCommentsTest(BaseTest):
 		comments_after = avatar_footer.get_comment_amount()
 
 		self.assertEqual(comments_before, comments_after)
+
+	def test_remove_and_add_and_reset_comment(self):
+		avatar_footer = self.user_avatar.comments
+		comments_before = avatar_footer.get_comment_amount()
+
+		avatar_footer.last_comment.delete_comment()
+		avatar_footer.last_comment.reset_comment()
+		avatar_footer.add_comment_to_avatar('New test comment')
+
+		print comments_before, self.user_avatar.comments.get_comment_amount()
+
+		WebDriverWait(avatar_footer, 5, 0.1).until(
+			lambda d: d.get_comment_amount() == comments_before + 1
+		)
+		comments_after = avatar_footer.get_comment_amount()
+
+		self.assertEqual(comments_before, comments_after - 1)
