0bcd2e756625a3bb8e02e9d9efd6c4f4e555a590
vladpereskokov <vladislavpereskokov@yandex.ru>
2017-11-15 14:42:06 +0300

    fix :bug: with imports

--- vp_check_list/pages/user.py
+++ vp_check_list/pages/user.py
@@ -2,10 +2,9 @@
 
 import os
 
-from selenium.webdriver.support.wait import WebDriverWait
-
 from vp_check_list.elements.avatar import UserAvatar
-from vp_check_list.pages.base import Page, Component
+from vp_check_list.elements.user import UserHeader, AuthForm
+from vp_check_list.pages.base import Page
 
 
 class AuthPage(Page):
@@ -23,30 +22,6 @@ class AuthPage(Page):
 		auth_form.set_login(user_login)
 		auth_form.set_password(password)
 		auth_form.submit()
-
-
-class AuthForm(Component):
-	LOGIN = '//input[@id="field_email"]'
-	PASSWORD = '//input[@id="field_password"]'
-	LOGIN_BUTTON = '//input[@class="button-pro __wide"]'
-
-	def set_login(self, login):
-		self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
-
-	def set_password(self, pwd):
-		self.driver.find_element_by_xpath(self.PASSWORD).send_keys(pwd)
-
-	def submit(self):
-		self.driver.find_element_by_xpath(self.LOGIN_BUTTON).click()
-
-
-class UserHeader(Component):
-	USERNAME = '//h1[@class="mctc_name_tx bl"]'
-
-	def get_username(self):
-		return WebDriverWait(self.driver, 5, 0.1).until(
-			lambda d: d.find_element_by_xpath(self.USERNAME).text
-		)
 
 
 class UserPage(Page):
