d059e621618cc6ca80a13169a5ecb61f907e7748
vladpereskokov <vladislavpereskokov@yandex.ru>
2017-11-18 08:58:42 +0300

    add new test

--- vp_check_list/tests/errors_tests.py
+++ vp_check_list/tests/errors_tests.py
@@ -9,13 +9,13 @@ class CombineActionsCommentsTest(BaseTest):
 		super(CombineActionsCommentsTest, cls).setUpClass()
 		cls.user_avatar.open_avatar()
 
-	def test_add_and_delete_comment(self):
+	def test_like_deleted_comment(self):
 		avatar_footer = self.user_avatar.comments
 
-		comment_before = avatar_footer.last_comment.text()
-		avatar_footer.add_comment_to_avatar(self.TEST_COMMENT)
-
-		avatar_footer.delete_comment_from_avatar()
-		comment_after = avatar_footer.last_comment.text()
+		last_comment = avatar_footer.last_comment
+		comment_before = last_comment.is_like()
+		last_comment.delete_comment()
+		last_comment.like()
+		comment_after = last_comment.is_like()
 
 		self.assertEqual(comment_before, comment_after)
