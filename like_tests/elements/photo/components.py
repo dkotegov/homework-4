01086a0e32e7a61033f5d66da397394e2e31fb9e
Semenov <tochka313@gmail.com>
2017-11-22 00:20:24 +0300

    Updated add photo button

--- like_tests/elements/photo/components.py
+++ like_tests/elements/photo/components.py
@@ -4,11 +4,11 @@ from like_tests.elements.component import Component
 
 
 class PhotoUploadButton(Component):
-    # BUTTON = '//input[@class="html5-upload-link __before-upload"]'
     BUTTON = '//input[@type="file"][@name="photo"]'
     ALBUM = '//a[@hrefattrs="st.cmd=userPersonalPhotos"]'
     PHOTO = '//a[@class="photo-card_cnt"][@href!="{}"]'
+    MOVE_PANEL = '//a[@data-l="aid,PhotoUpload_ShowMovePanel"]'
 
     def load_photo(self, photo_path):
         self.driver.find_element_by_xpath(self.BUTTON).send_keys(photo_path)
-        self.driver.find_element_by_xpath(self.ALBUM).click()
+        self.driver.find_element_by_xpath(self.MOVE_PANEL)
