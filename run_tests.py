d28b36583d9349664f07aa783bf21f61e45e92b7
Ed Asriyan <ed-asriyan@protonmail.com>
2017-11-26 16:58:08 +0300

    Create MessageTest

--- run_tests.py
+++ run_tests.py
@@ -3,8 +3,11 @@
 import sys
 import unittest
 
+from tests.messages import MessagesTest
+
 if __name__ == '__main__':
     suite = unittest.TestSuite((
+        unittest.makeSuite(MessagesTest),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
