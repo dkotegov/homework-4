212619169ae0f0aa329d0cbaccd68998bfb10dbe
serbut <serbut555@gmail.com>
2017-11-27 18:51:05 +0300

    Tests

---
+++ tests_butorin/base_test.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+
+from selenium.webdriver import DesiredCapabilities, Remote
+
+from page import AuthPage, WAIT_TIME
+
+LOGIN = os.environ['LOGIN']
+PASSWORD = os.environ['PASSWORD']
+
+
+class BaseTest(unittest.TestCase):
+
+    def setUp(self):
+        browser = os.environ.get('BROWSER', 'FIREFOX')
+
+        self.driver = Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=getattr(DesiredCapabilities, browser).copy()
+        )
+        self.driver.implicitly_wait(WAIT_TIME)
+
+        self.login()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def login(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+
+        auth_form = auth_page.form
+        auth_form.set_login(LOGIN)
+        auth_form.set_password(PASSWORD)
+        auth_form.submit()
+
