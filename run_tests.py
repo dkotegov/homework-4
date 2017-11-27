1ba9e1f8f009332c34f38b4a35220de69cd9eb20
Alexandr Orel <4aorel@mail.ru>
2017-11-27 19:08:04 +0300

    fix

--- run_tests.py
+++ run_tests.py
@@ -7,10 +7,7 @@ from tests_butorin.photos_test import butorin_tests
 
 if __name__ == '__main__':
 
-    # for test_suite in tests_orel:
-    #     unittest.TextTestRunner().run(test_suite)
-
-    for test_suite in butorin_tests:
+    for test_suite in tests_orel:
         unittest.TextTestRunner().run(test_suite)
 
     for test_suite in butorin_tests:
