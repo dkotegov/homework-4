895eb3393313cfb2ecbb17c25c9631fe3d47b213
pacman29 <balyakin.d.v@gmail.com>
2017-11-28 10:59:58 +0300

    add open tabs tests

--- tests/discussions.py
+++ tests/discussions.py
@@ -9,32 +9,32 @@ class DiscussionsTest(BaseTest):
     def test_discussions_open(self):
         discussions_page = DiscussionsPage(self.driver)
         discussions_page.navigate()
-        self.assertTrue(discussions_page.is_opened(), 'discussions opened')
+        self.assertTrue(discussions_page.is_opened(), 'discussions not opened')
 
     def test_tab_participated_open(self):
         dp = DiscussionsPage(self.driver)
         dp.navigate()
         elm1 = dp.openParticipatedTab()
         elm2 = dp.selectedTab()
-        self.assertEquals(elm1,elm2)
+        self.assertEquals(elm1,elm2,"Participated not opened")
 
     def test_tab_my_posts_open(self):
         dp = DiscussionsPage(self.driver)
         dp.navigate()
         elm1 = dp.openMyPostsTab()
         elm2 = dp.selectedTab()
-        self.assertEquals(elm1,elm2)
+        self.assertEquals(elm1,elm2,"My posts not opened")
 
     def test_tab_friends_open(self):
        dp = DiscussionsPage(self.driver)
        dp.navigate()
        elm1 = dp.openFriendsTab()
        elm2 = dp.selectedTab()
-       self.assertEquals(elm1,elm2)
+       self.assertEquals(elm1,elm2,"Friends not opened")
 
     def test_tab_groups_open(self):
         dp = DiscussionsPage(self.driver)
         dp.navigate()
         elm1 = dp.openGrupsTab()
         elm2 = dp.selectedTab()
-        self.assertEquals(elm1, elm2)
+        self.assertEquals(elm1, elm2,"Groups not opened")
