d6079df402a7d7cb90b75fafb3c09806bb906f69
wolf1996 <ksgiv37@gmail.com>
2017-11-28 19:37:07 +0300

    Перенесены мои тесты

--- tests/discussions.py
+++ tests/discussions.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from base import BaseTest
-
+from tests.pages.discussions.pages import DiscPage
 
 class DiscussionsTest(BaseTest):
-    pass
+    def setUp(self):
+        super(DiscussionsTest, self).setUp()
+        drv = self.driver
+        self.page = DiscPage(drv)
+
+    def test_visible_user_card(self):
+        self.page.check_visible_user_info()
+        pass
+
+    def test_visible_button_card(self):
+        self.page.downbutton_showed()
+        pass
+
+    def test_visible_classlist(self):
+        self.page.class_showed()
+        pass
+
+    def test_visible_warn(self):
+        self.page.warn_showed()
+        pass
+
+    def test_visible_time(self):
+        self.page.time_showed()
+        pass
