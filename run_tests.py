c4aaacdb235fa86e71f2b8d1e029f923618e04e2
Konstantin Gulyy <kgulyy@my.com>
2018-05-07 14:29:57 +0300

    Auth test

--- run_tests.py
+++ run_tests.py
@@ -2,11 +2,14 @@
 
 import sys
 import unittest
+
+from tests.auth_test import AuthTest
 from tests.shop_tests import ShopTests
 from tests.post_tests import PostTests
 
 if __name__ == '__main__':
     suite = unittest.TestSuite([
+        # unittest.makeSuite(AuthTest),
         unittest.makeSuite(ShopTests),
         unittest.makeSuite(PostTests)
     ])
---
+++ tests/auth_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from PageObjects.page_objects import MainPage
+from tests.common import getDriver, Auth
+
+
+class AuthTest(unittest.TestCase):
+    USERNAME = u'Феофан Лампер'
+
+    def setUp(self):
+        self.driver = getDriver()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test(self):
+        auth = Auth(self.driver)
+        auth.auth()
+
+        main_page = MainPage(self.driver)
+        left_menu = main_page.left_menu
+        username = left_menu.get_username()
+
+        self.assertEqual(self.USERNAME, username)
