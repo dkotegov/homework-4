b106b14a358f3ef1c87621c27cab4b3467d27bfa
Nikita-Boyarskikh <N02@yandex.ru>
2018-05-16 10:18:55 +0300

    Remove examples

--- basic.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from selenium import webdriver
-
-if __name__ == '__main__':
-    driver = webdriver.Chrome('./chromedriver')
-    # driver = webdriver.Firefox(executable_path='./geckodriver')
-    driver.get("https://ok.ru/")
-    driver.quit()
