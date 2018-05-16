95148ee2e51f658c5e854cfc3f9d334bce9740c1
Nikita-Boyarskikh <N02@yandex.ru>
2018-05-16 10:24:06 +0300

    Update run_tests.py to run all tests/*_test.py files

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
-# !/usr/bin/env python3
-
+#!/usr/bin/env python3
 import sys
-import unittest
-import test
+from os.path import join, dirname, realpath
+from unittest import TestLoader
+
+from config import TESTS_DIR, TEST_PATTERN
+from library.selenium import ScreenshotTextTestRunner
 
 if __name__ == '__main__':
-    suite = unittest.TestSuite((
-        unittest.makeSuite(test.ExampleTest),
-    ))
-    result = unittest.TextTestRunner().run(suite)
+    loader = TestLoader()
+    tests_dir = join(dirname(realpath(__file__)), TESTS_DIR)
+    tests = loader.discover(tests_dir, pattern=TEST_PATTERN)
+    runner = ScreenshotTextTestRunner()
+    result = runner.run(tests)
     sys.exit(not result.wasSuccessful())
