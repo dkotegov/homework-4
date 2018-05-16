8442e6099dca2999f0ade84b3cd87f193858470d
Konstantin Gulyy <kgulyy@my.com>
2018-05-16 14:44:24 +0300

    Return frequency for waiting

--- Components/component.py
+++ Components/component.py
@@ -6,27 +6,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 class Component(object):
     TIMEOUT = 30
+    FREQUENCY = 0.1
 
     def __init__(self, driver):
         self.driver = driver
 
     def click_element(self, element):
-        WebDriverWait(self.driver, self.TIMEOUT).until(
+        WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
             lambda d: d.find_element_by_xpath(element)
         ).click()
 
     def input_text_to_element(self, element, text):
-        WebDriverWait(self.driver, self.TIMEOUT).until(
+        WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
             lambda d: d.find_element_by_xpath(element)
         ).send_keys(text)
 
     def upload_image(self, element, file_name):
         image_path = os.getcwd() + '/images/' + file_name
-        WebDriverWait(self.driver, self.TIMEOUT).until(
+        WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
             lambda d: d.find_element_by_xpath(element)
         ).send_keys(image_path)
 
     def get_element_text(self, element):
-        return WebDriverWait(self.driver, self.TIMEOUT).until(
+        return WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
             lambda d: d.find_element_by_xpath(element).text
         )
