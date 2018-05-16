e9025c40cfe0f32ad37f1e140e5946041dd6f793
Paulik8 <ikhotin@mail.ru>
2018-05-16 20:32:13 +0300

    example wait

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -12,6 +12,7 @@ class AuthPage(Page):
         auth_form = AuthForm(self.driver)
         auth_form.get_login().send_keys(login)
         auth_form.get_password().send_keys(password)
+        # auth_form.get_example_problem_button().click()
         auth_form.submit().click()
 
 
