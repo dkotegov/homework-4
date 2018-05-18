a1361285a9addaf62af986c05f558abd2f4492a4
Konstantin Gulyy <kgulyy@my.com>
2018-05-18 07:59:32 +0300

    Refactoring component methods

--- Components/component.py
+++ Components/component.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
-
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,37 +12,32 @@ class Component(object):
     def __init__(self, driver):
         self.driver = driver
 
-    def is_exist_element(self, element):
+    def find_element(self, xpath):
+        return WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
+            lambda d: d.find_element_by_xpath(xpath)
+        )
+
+    def is_exist_element(self, xpath):
         try:
-            WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
-                lambda d: d.find_element_by_xpath(element)
-            )
+            self.find_element(xpath)
         except TimeoutException:
             return False
         return True
 
-    def click_element(self, element):
-        WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
-            lambda d: d.find_element_by_xpath(element)
-        ).click()
+    def click_element(self, xpath):
+        self.find_element(xpath).click()
 
-    def input_text_to_element(self, element, text):
-        input_element = WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
-            lambda d: d.find_element_by_xpath(element)
-        )
+    def input_text_to_element(self, xpath, text):
+        input_element = self.find_element(xpath)
         input_element.clear()
         input_element.send_keys(text)
 
-    def upload_image(self, element, file_name):
+    def upload_image(self, xpath, file_name):
         image_path = '../images/' + file_name
-        WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
-            lambda d: d.find_element_by_xpath(element)
-        ).send_keys(image_path)
+        self.find_element(xpath).send_keys(image_path)
 
-    def get_element_text(self, element):
-        return WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY).until(
-            lambda d: d.find_element_by_xpath(element).text
-        )
+    def get_element_text(self, xpath):
+        return self.find_element(xpath).text
 
     def waiting_until_invisible(self, element):
         wait = WebDriverWait(self.driver, self.TIMEOUT, self.FREQUENCY)
