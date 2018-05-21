96cc0a352f38ee3990c96299552ae23208e731e7
Alexander <kiryanenkoav@mail.ru>
2018-05-21 08:37:24 +0300

    Refactor

--- tests/mobile_album_test.py
+++ tests/mobile_album_test.py
@@ -30,40 +30,35 @@ class AlbumTest(unittest.TestCase):
 
         AuthPage(self.driver).auth(self.LOGIN, self.PASSWORD)
 
+        self.album_name = 'Test album #{}'.format(time.time())
+        UserAlbumEditPage(self.driver).create_album(self.album_name)
+
+        self.album_page = UserAlbumPage(self.driver)
+
     def tearDown(self):
         self.driver.quit()
 
     def upload_photo_and_open(self):
-        album_page = UserAlbumPage(self.driver)
-        album_id = album_page.parse_album_id()
+        album_id = self.album_page.parse_album_id()
 
         UserAddAlbumPhotoPage(self.driver, album_id).upload_photo()
 
-        album_page.open()
-        photos_list = album_page.photos_list
+        self.album_page.open()
+        photos_list = self.album_page.photos_list
         photos_list.first.open()
 
     def test_remove_album(self):
-        album_name = 'Test album #{} for remove'.format(time.time())
-        UserAlbumEditPage(self.driver).create_album(album_name)
-
-        album_page = UserAlbumPage(self.driver)
-
-        toolbar = album_page.toolbar
+        toolbar = self.album_page.toolbar
         toolbar.open()
         toolbar.delete()
 
-        album_page.confirmation_modal.delete()
+        self.album_page.confirmation_modal.delete()
 
         albums_list = UserAlbumsPage(self.driver).albums_list
-        self.assertFalse(albums_list.includes(album_name))
+        self.assertFalse(albums_list.includes(self.album_name))
 
     def test_rename_album(self):
-        UserAlbumEditPage(self.driver).create_album()
-
-        album_page = UserAlbumPage(self.driver)
-
-        toolbar = album_page.toolbar
+        toolbar = self.album_page.toolbar
         toolbar.open()
         toolbar.edit()
 
@@ -72,46 +67,36 @@ class AlbumTest(unittest.TestCase):
         edit_form.set_name(album_name)
         edit_form.submit()
 
-        self.assertEqual(album_name, album_page.empty_album.title)
+        self.assertEqual(album_name, self.album_page.empty_album.title)
 
     def test_like_album(self):
-        album_name = 'Liked test album #{}'.format(time.time())
-        UserAlbumEditPage(self.driver).create_album(album_name)
-        UserAlbumsPage(self.driver).like_album(album_name)
+        UserAlbumsPage(self.driver).like_album(self.album_name)
 
         albums_page = UserAlbumsPage(self.driver)
-        album_item = albums_page.albums_list.find(album_name)
+        album_item = albums_page.albums_list.find(self.album_name)
         self.assertEqual(1, album_item.likes_count)
 
     def test_cancel_album_like(self):
-        album_name = 'Liked test album #{}'.format(time.time())
-        UserAlbumEditPage(self.driver).create_album(album_name)
-        UserAlbumsPage(self.driver).like_album(album_name)
+        UserAlbumsPage(self.driver).like_album(self.album_name)
 
         # Дизлайк
-        UserAlbumsPage(self.driver).like_album(album_name)
+        UserAlbumsPage(self.driver).like_album(self.album_name)
 
         albums_page = UserAlbumsPage(self.driver)
-        album_item = albums_page.albums_list.find(album_name)
+        album_item = albums_page.albums_list.find(self.album_name)
         self.assertEqual(0, album_item.likes_count)
 
     def test_add_photo(self):
-        UserAlbumEditPage(self.driver).create_album()
-
-        album_page = UserAlbumPage(self.driver)
-        album_id = album_page.parse_album_id()
+        album_id = self.album_page.parse_album_id()
 
         UserAddAlbumPhotoPage(self.driver, album_id).upload_photo()
 
         edit_photo = UserEditAlbumPhotoPage(self.driver)
         edit_photo.form.save()
-        self.assertEqual(1, album_page.photos_list.count)
+        self.assertEqual(1, self.album_page.photos_list.count)
 
     def test_add_photo_with_description(self):
-        UserAlbumEditPage(self.driver).create_album()
-
-        album_page = UserAlbumPage(self.driver)
-        album_id = album_page.parse_album_id()
+        album_id = self.album_page.parse_album_id()
 
         UserAddAlbumPhotoPage(self.driver, album_id).upload_photo()
 
@@ -120,14 +105,13 @@ class AlbumTest(unittest.TestCase):
         edit_photo.set_description(description)
         edit_photo.save()
 
-        photos_list = album_page.photos_list
+        photos_list = self.album_page.photos_list
         self.assertEqual(1, photos_list.count)
         photos_list.first.open()
         photo = PhotoPage(self.driver).photo
         self.assertEqual(description, photo.description)
 
     def test_like_photo(self):
-        UserAlbumEditPage(self.driver).create_album()
         self.upload_photo_and_open()
 
         photo = PhotoPage(self.driver).photo
@@ -135,7 +119,6 @@ class AlbumTest(unittest.TestCase):
         self.assertEqual(1, photo.likes_count)
 
     def test_cancel_photo_like(self):
-        UserAlbumEditPage(self.driver).create_album()
         self.upload_photo_and_open()
 
         photo = PhotoPage(self.driver).photo
@@ -146,16 +129,13 @@ class AlbumTest(unittest.TestCase):
         self.assertEqual(0, photo.likes_count)
 
     def test_make_photo_album_cover(self):
-        UserAlbumEditPage(self.driver).create_album()
-
-        album_page = UserAlbumPage(self.driver)
-        album_id = album_page.parse_album_id()
+        album_id = self.album_page.parse_album_id()
 
         UserAddAlbumPhotoPage(self.driver, album_id).upload_photo(abspath('tests/photos/test_photo.jpg'))
         UserAddAlbumPhotoPage(self.driver, album_id).upload_photo(abspath('tests/photos/test_photo2.jpeg'))
 
-        album_page.open()
-        photos_list = album_page.photos_list
+        self.album_page.open()
+        photos_list = self.album_page.photos_list
 
         # Делаю фото обложкой
         photo_item = photos_list.get(1)
@@ -164,5 +144,5 @@ class AlbumTest(unittest.TestCase):
         photo_item.open()
         PhotoPage(self.driver).make_photo_cover()
 
-        album_page.open()
-        self.assertEqual(photo_id, album_page.album_header.cover_id)
+        self.album_page.open()
+        self.assertEqual(photo_id, self.album_page.album_header.cover_id)
