3601599ee77b0a79a12dcb2a7bba81714f5b36e5
MikeGus <vfc17@yandex.ru>
2018-05-24 23:41:04 +0300

    rm alltests.py

--- run_tests.py
+++ run_tests.py
@@ -4,10 +4,8 @@ import unittest
 
 from test_mikegus import TestsMikeGus
 from tests import Tests
-from alltests import TestsBuevichGusev
 
 if __name__ == '__main__':
     #suite = unittest.TestSuite((unittest.makeSuite(Tests)),)
-    # suite1 = unittest.TestSuite((unittest.makeSuite(TestsBuevichGusev)),)
     suite2 = unittest.TestSuite((unittest.makeSuite(TestsMikeGus)))
     result = unittest.TextTestRunner().run(suite2)
