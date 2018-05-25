c68c472a8b5b49d9cda79fa9574d71d9435736b3
zwirec <zwirec@icloud.com>
2018-05-25 15:45:58 +0300

    run_tests

--- run_tests.py
+++ run_tests.py
@@ -1,19 +1,7 @@
 import sys
 import unittest
 
-from tests.test_settings import *
-
 if __name__ == '__main__':
-    suite = unittest.TestSuite((
-        # unittest.makeSuite(TestsAddApp),
-        # unittest.makeSuite(TestsChangeNameAndDescriptions),
-        unittest.makeSuite(TestsAddStuff),
-        # unittest.makeSuite(TestsAddGroupLinks),
-        # unittest.makeSuite(TestsChangeType),
-        # unittest.makeSuite(TestsHideSections),
-        # unittest.makeSuite(TestsForbiddenComments),
-        # unittest.makeSuite(TestsAddAPIKey),
-        # unittest.makeSuite(TestsHideObsceneLanguage)
-    ))
-    result = unittest.TextTestRunner(failfast=True, verbosity=2).run(suite)
+    suite = unittest.TestLoader().discover('tests')
+    result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
