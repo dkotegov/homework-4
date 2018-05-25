0dc0ea3a7cdc9ca4d205c6a378bbfbb4ea7ba89e
MikeGus <vfc17@yandex.ru>
2018-05-25 07:55:11 +0300

    fixed fetches

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,6 @@ from tests import Tests
 from tests_privacy import TestsPrivacy
 
 
-
 if __name__ == '__main__':
     #suite = unittest.TestSuite((unittest.makeSuite(Tests)),)
     suite2 = unittest.TestSuite((unittest.makeSuite(TestsMikeGus)))
