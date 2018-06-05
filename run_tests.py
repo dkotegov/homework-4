2a925f288aeabf1fe16cd6918d4ef3d03c1c0c6a
MikeGus <vfc17@yandex.ru>
2018-06-05 21:21:42 +0300

    Fixed runtests

--- run_tests.py
+++ run_tests.py
@@ -9,13 +9,10 @@ from test_johnkeats97 import TestsBaseSettings
 
 
 if __name__ == '__main__':
-    #suite = unittest.TestSuite((unittest.makeSuite(Tests)),)
-    suite2 = unittest.TestSuite((unittest.makeSuite(TestsMikeGus)))
-    #suite3 = unittest.TestSuite((unittest.makeSuite(TestsPrivacy)))
-    # suite = unittest.TestSuite((
-    #     unittest.makeSuite(TestsMikeGus),
-    #     unittest.makeSuite(Tests),
-    #     unittest.makeSuite(TestsPrivacy),
-    #     unittest.makeSuite(TestsBaseSettings),
-    # ))
-    result = unittest.TextTestRunner().run(suite2)
+    suite = unittest.TestSuite((
+        unittest.makeSuite(TestsMikeGus),
+        unittest.makeSuite(Tests),
+        unittest.makeSuite(TestsPrivacy),
+        unittest.makeSuite(TestsBaseSettings),
+    ))
+    result = unittest.TextTestRunner().run(suite)
