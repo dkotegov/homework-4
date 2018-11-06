3924bee469f6972cfd8c7802ad323cbdb126d649
chapay <BritikovKI@Yandex.ru>
2018-11-06 11:03:39 +0300

    better late then never

---
+++ tests/letter_functions/base_send.py
@@ -0,0 +1,8 @@
+from pages.letter_formatting_page import LetterFormattingPage
+from tests.base_test import BaseTest
+
+class BaseSend(BaseTest):
+    def test(self):
+        BaseTest.test(self)
+        self.file_attaching_page = LetterFormattingPage(self.driver)
+        self.file_attaching_form = self.file_attaching_page.form
