28d21655863dbf5b685d19cda205d9bacf2c70f4
InfantryMan <rust-skate@mail.ru>
2018-11-12 14:34:56 +0300

    Added hub + nodes

--- tests/Poponkin/test.py
+++ tests/Poponkin/test.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from tests.pages.auth_page import AuthPage
 from tests.pages.main_page import MainPage
 
+from selenium.webdriver import DesiredCapabilities, Remote
+
 class Test(unittest.TestCase):
     USEREMAIL = 'ttexnopark@mail.ru'
     PASSWORD = os.environ['PASSWORD']
@@ -13,12 +15,12 @@ class Test(unittest.TestCase):
     def setUp(self):
         browser = os.environ.get('BROWSER', 'CHROME')
 
-        self.driver = webdriver.Chrome('./chromedriver')
+        # self.driver = webdriver.Chrome('./chromedriver')
 
-        # self.driver = Remote(
-        #     command_executor='http://127.0.0.1:4444/wd/hub',
-        #     desired_capabilities=getattr(DesiredCapabilities, browser).copy()
-        # )
+        self.driver = Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=getattr(DesiredCapabilities, browser).copy()
+        )
 
     def tearDown(self):
         self.driver.quit()
