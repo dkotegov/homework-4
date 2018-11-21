735b2edc8049b9df5cda99124036dd4481c16cee
Danchetto <danchetto@yandex.ru>
2018-11-21 18:17:20 +0300

    + selecting all messages & drag'n'drop

--- tests/example.py
+++ tests/example.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import unittest
 
 
 if __name__ == '__main__':
@@ -34,8 +36,24 @@ if __name__ == '__main__':
         password_input.send_keys(user_data['password'])
 
         continue_button.click()
+
+        driver.switch_to_default_content()
         
+        # checkboxes = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'b-datalist__item__cbx')))
+
+        all_checkbox = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-name="toggle"] > .b-checkbox__box')))
+        all_checkbox.click()
+        
+        # for cbx in checkboxes:
+        #     cbx.click()
+
+        messages = driver.find_elements_by_class_name('b-datalist__item__panel')[0]
+        target_dir = driver.find_element_by_css_selector('div[data-id="500001"]')
+
+        ActionChains(driver).drag_and_drop(messages, target_dir).perform()
+
         time.sleep(3)
+        
     
     finally:
         driver.close()
