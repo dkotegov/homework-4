f34620c4d27f884bb1b2d8a3f958ecd407b25b2a
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-12-09 23:19:27 +0300

    del comments and run all

--- run_tests.py
+++ run_tests.py
@@ -8,9 +8,8 @@ from tests.changeFilterTests import ChangeFilterTest
 from tests.errorCheckingTests import ErrorCheckingTest
 
 if __name__ == '__main__':
-    #create_filters_test = unittest.TestLoader().loadTestsFromTestCase(CreateFilterTest)
-    #change_filter_test = unittest.TestLoader().loadTestsFromTestCase(ChangeFilterTest)
+    create_filters_test = unittest.TestLoader().loadTestsFromTestCase(CreateFilterTest)
+    change_filter_test = unittest.TestLoader().loadTestsFromTestCase(ChangeFilterTest)
     error_checking_test = unittest.TestLoader().loadTestsFromTestCase(ErrorCheckingTest)
-    #suite = unittest.TestSuite([create_filters_test, change_filter_test, error_checking_test])
-    suite = unittest.TestSuite([error_checking_test])
+    suite = unittest.TestSuite([create_filters_test, change_filter_test, error_checking_test])
     result = unittest.TextTestRunner().run(suite)
