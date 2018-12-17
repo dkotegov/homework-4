8f5402f46d630f9082dc8b66c4357c02fced56d5
eugenmorozov <eugen.letterbox@mail.ru>
2018-12-17 15:50:43 +0300

    deleted some files

--- tests/pages/auth-page.py
+++
@@ -1,9 +0,0 @@
-from page import Page
-from tests.components.auth_form import AuthForm
-
-class AuthPage(Page):
-    PATH = ''
-
-    @property
-    def form(self):
-        return AuthForm(self.driver)
