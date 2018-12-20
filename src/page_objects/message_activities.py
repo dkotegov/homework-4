47698612679fe3615c49fe106cd2a0fd4b6d465c
Danchetto <danchetto@yandex.ru>
2018-12-20 19:50:33 +0300

    + consts

--- src/page_objects/message_activities.py
+++ src/page_objects/message_activities.py
@@ -5,14 +5,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 
+MESSAGE_TITLE = '.ll-sj__normal'
+DATASET_ITEMS = '.dataset__items'
+MESSAGE = '.llc_normal'
+MESSAGE_AVATAR = '.llc__avatar'
+SELECT_ALL = 'span[title="Выделить все (Ctrl+A)"]'
+MOVE_TO_FOLDER = 'span[title="В папку (V)"]'
+FOLDERS = '.nav-folders'
+MORE = 'span[title="Ещё (.)"]'
+DROPDOWN_MENU = '.dropdown__menu'
+REMOVE_SELECTION = 'span[title="Снять выделение (Ctrl+A)"]'
+FILTERS = '.filters-control'
+FILTER_ITEMS = '.select__items'
+FILTER_TEXT = '.filters-control__filter-text'
+
 
 class MessageActivities(PageObject):
-
     def get_message_title(self, msg):
         try:
-            return msg.find_element_by_css_selector('.ll-sj__normal').text
+            return msg.find_element_by_css_selector(MESSAGE_TITLE).text
         except StaleElementReferenceException:
-            return msg.find_element_by_css_selector('.ll-sj__normal').text
+            return msg.find_element_by_css_selector(MESSAGE_TITLE).text
 
     def get_all_titles(self, data):
         titles = []
@@ -22,8 +35,8 @@ class MessageActivities(PageObject):
         return titles
 
     def get_messages(self):
-        data = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'dataset__items')))
-        messages = data.find_elements_by_css_selector('.llc_normal')
+        data = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, DATASET_ITEMS)))
+        messages = data.find_elements_by_css_selector(MESSAGE)
 
         return messages, len(messages)
 
@@ -32,8 +45,8 @@ class MessageActivities(PageObject):
         titles = self.get_all_titles(messages)
 
         if msg_count:
-            messages[0].find_element_by_css_selector('.llc__avatar').click()
-            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[title="Выделить все (Ctrl+A)"]'))).click()
+            messages[0].find_element_by_css_selector(MESSAGE_AVATAR).click()
+            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, SELECT_ALL))).click()
             self.__move(destination)
             self.wait_until_moved(messages[0])
 
@@ -51,7 +64,7 @@ class MessageActivities(PageObject):
             for i in range(0, n):
                 result.append(messages[msg_count - i - 1])
                 titles.append(self.get_message_title(result[-1]))
-                messages[msg_count - i - 1].find_element_by_css_selector('.llc__avatar').click()
+                messages[msg_count - i - 1].find_element_by_css_selector(MESSAGE_AVATAR).click()
 
             self.__move(destination)
 
@@ -59,7 +72,6 @@ class MessageActivities(PageObject):
         return result, titles
 
     def wait_until_moved(self, msg):
-        # self.wait.until(EC.presence_of_element_located(msg.locator))
         msg_id = self.driver.execute_script('return arguments[0].attributes["data-id"].value', msg)
         try:
             while (self.driver.find_element_by_css_selector('a[data-id="{}"]').format(msg_id)):
@@ -68,10 +80,9 @@ class MessageActivities(PageObject):
             return True
 
     def wait_until_appear(self):
-        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.llc_normal')))
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, MESSAGE)))
 
     def check_moved_messages(self, titles):
-        
         messages = self.get_messages()[0]
         message_titles = self.get_all_titles(messages)
         
@@ -84,11 +95,11 @@ class MessageActivities(PageObject):
         return result
 
     def __move(self, destination):
-        self.driver.find_element_by_css_selector('span[title="В папку (V)"]').click()
+        self.driver.find_element_by_css_selector(MOVE_TO_FOLDER).click()
         self.driver.find_element_by_css_selector('a[title="{}"]'.format(destination)).click()
 
     def go_to(self, destination):
-        folders = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.nav-folders')))
+        folders = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, FOLDERS)))
         folders.find_element_by_css_selector('a[title="{}"]'.format(destination)).click()
         self.wait_until_appear()
 
@@ -101,13 +112,13 @@ class MessageActivities(PageObject):
 
         if msg_count:
             for i in range(0, n):
-                messages[i].find_element_by_css_selector('.llc__avatar').click()
+                messages[i].find_element_by_css_selector(MESSAGE_AVATAR).click()
                 result.append(messages[i])
-            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[title="Ещё (.)"]'))).click()
-            self.driver.find_element_by_css_selector('.dropdown__menu')
+            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, MORE))).click()
+            self.driver.find_element_by_css_selector(DROPDOWN_MENU)
             self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.list-item__ico_{}'.format(flag_type)))).click()
             for i in range(0, n):
-                messages[i].find_element_by_css_selector('.llc__avatar').click()
+                messages[i].find_element_by_css_selector(MESSAGE_AVATAR).click()
 
         return result
 
@@ -115,17 +126,17 @@ class MessageActivities(PageObject):
         messages, msg_count = self.get_messages()
         
         if msg_count:
-            messages[0].find_element_by_css_selector('.llc__avatar').click()
-            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[title="Выделить все (Ctrl+A)"]'))).click()
-            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[title="Ещё (.)"]'))).click()
-            self.driver.find_element_by_css_selector('.dropdown__menu')
+            messages[0].find_element_by_css_selector(MESSAGE_AVATAR).click()
+            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, SELECT_ALL))).click()
+            self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, MORE))).click()
+            self.driver.find_element_by_css_selector(DROPDOWN_MENU)
             self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.list-item__ico_{}'.format(flag_type)))).click()
             self.remove_selection()
 
         return messages
 
     def remove_selection(self):
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span[title="Снять выделение (Ctrl+A)"]'))).click()
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, REMOVE_SELECTION))).click()
 
     def show_by_filter(self, flag_type):
         if flag_type == 'unread':
@@ -133,8 +144,8 @@ class MessageActivities(PageObject):
         elif flag_type == 'all':
             flag_type = False
         
-        self.driver.find_element_by_css_selector('.filters-control').click()
-        filters_menu = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.select__items')))
+        self.driver.find_element_by_css_selector(FILTERS).click()
+        filters_menu = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, FILTER_ITEMS)))
         if flag_type:
             filters_menu.find_element_by_css_selector('.ico_16-{}'.format(flag_type)).click()
         else:
