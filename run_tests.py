01fa4417334bf100b62afdb5221f0c67debd42f5
kate <kirilek@bk.ru>
2019-11-14 19:32:29 +0300

    init

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from selenium import webdriver
 
 if __name__ == '__main__':
-    pass
+    driver = webdriver.Firefox()
+    driver.get("https://park.mail.ru/")
+    driver.quit()
