17b03f2ce18297e98e0daf0d57e01af6a168232f
Temirlan <panda13956@gmail.com>
2019-11-16 00:12:19 +0300

    1 init test

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
+from selenium import webdriver
 
 import unittest
+import os
+import time
+
+class AskTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome('./chromedriver')
+        self.driver.get('https://otvet.mail.ru/ask')
+        self.username = 'test_qwerty1122@mail.ru'
+        self.password = os.getenv('PASSWORD')
+
+    def test_01(self):
+        driver = self.driver
+
+        input = driver.find_elements_by_class_name('B1kkDlxig1yJhDIKljYs2_0')
+
+
+    def tearDown(self):
+        self.driver.quit() 
 
 if __name__ == '__main__':
+    unittest.main()    
     pass
+
+
