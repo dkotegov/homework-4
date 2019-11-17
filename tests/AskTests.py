d6bb7696cd2c81846d7e6e45c979aec68457b514
Temirlan <panda13956@gmail.com>
2019-11-17 03:59:41 +0300

    5 test added

--- tests/AskTests.py
+++ tests/AskTests.py
@@ -14,32 +14,32 @@ class AskTests(unittest.TestCase):
     def tearDown(self):
         self.page.quitDriver()
 
-    # def test_notEmptyQuestion(self):
-    #     shortQuestion = 'Why, man?'
-    #     self.page.setQuestionTheme(shortQuestion)
-    #     self.page.clearQuestionThemeByKeys()
-    #     self.assertEqual(self.page.getAlertUnderQuestion(),
-    #         'Поле «Тема вопроса» обязательно для заполнения.')
+    def test_notEmptyQuestion(self):
+        shortQuestion = 'Why, man?'
+        self.page.setQuestionTheme(shortQuestion)
+        self.page.clearQuestionThemeByKeys()
+        self.assertEqual(self.page.getAlertUnderQuestion(),
+            'Поле «Тема вопроса» обязательно для заполнения.')
 
-    # def test_tooBigQuestion(self):
-    #     bigStr = ''
-    #     for _ in range(122):
-    #         bigStr = bigStr + 'a'
-    #     self.page.setQuestionTheme(bigStr)
-    #     self.assertEqual(self.page.getAlertUnderQuestion(),
-    #         'Поле «Тема вопроса» не может быть больше 120 символов.')
+    def test_tooBigQuestion(self):
+        bigStr = ''
+        for _ in range(122):
+            bigStr = bigStr + 'a'
+        self.page.setQuestionTheme(bigStr)
+        self.assertEqual(self.page.getAlertUnderQuestion(),
+            'Поле «Тема вопроса» не может быть больше 120 символов.')
 
-    # def test_mentionCountry(self):
-    #     questionWithCountry = 'Россия'
-    #     self.page.setQuestionTheme(questionWithCountry)
-    #     # Пока не понятно как этот костыль решать
-    #     time.sleep(5)
-    #     self.assertEqual(self.page.getSubcategory(),
-    #         'Политика')
+    def test_mentionCountry(self):
+        questionWithCountry = 'Россия'
+        self.page.setQuestionTheme(questionWithCountry)
+        # Пока не понятно как этот костыль решать
+        time.sleep(5)
+        self.assertEqual(self.page.getSubcategory(),
+            'Политика')
 
-    # def test_loginBtn(self):
-    #     self.page.clickLogin()
-    #     self.assertTrue(self.page.lofinFormIsVisible())
+    def test_loginBtn(self):
+        self.page.clickLogin()
+        self.assertTrue(self.page.lofinFormIsVisible())
 
     def test_authorization(self):
         self.page.clickLogin()
