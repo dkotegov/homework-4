d5dc138c5dc343be63682f4cf060e18919c4ed5a
Flyewzz <dentalon599@gmail.com>
2019-12-01 14:59:28 +0300

    In progress, some features are done.

--- LoginPage.py
+++ LoginPage.py
@@ -1,21 +1,109 @@
 import os
 import unittest
 
-from selenium.webdriver import DesiredCapabilities, Remote
+from selenium.webdriver import DesiredCapabilities, Remote, ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+import time
 
 class LoginPage:
-    BASE_URL = 'https://m.calendar.mail.ru/login'
-
+    # BASE_URL = 'https://m.calendar.mail.ru/login'
+    
     def __init__(self, driver):
         self.driver = driver
-        self.driver.get(self.BASE_URL)
+        # self.driver.get(self.BASE_URL)
 
-    def open(self):
-        url = self.BASE_URL
+    def open(self, url):
+        # url = self.BASE_URL
         self.driver.get(url)
         self.driver.maximize_window()
+
+    def enter_login(self, login):
+        elem = self.driver.find_element_by_css_selector('input[name=Login]')
+        elem.send_keys(login)
+
+    def enter_password(self, password):
+        elem = self.driver.find_element_by_css_selector('input[name=Password]')
+        elem.send_keys(password)
+
+    def login(self):
+        elem = self.driver.find_element_by_css_selector('.login-button')
+        elem.click()
+
+    def enter_event_name(self, name):
+        elem = self.driver.find_element_by_css_selector('.event-summary-input')
+        # Clear old data
+        elem.clear()
+        # Enter new data
+        elem.send_keys(name)
+
+    def open_menu_plus(self):
+        # elem = self.driver.find_element_by_css_selector('.header-menu-item__list')
+        elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.header-menu-item.header-menu-item__sidebutton.header-menu-item__plus')))        
+        elem.click()
+
+
+    # def click_on_create_btn(self):
+    #     # elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.calendar-create-item')))
+    #     # # elem = self.driver.find_element_by_css_selector('div.calendar-create-item.panel-item.panel-item__marked')
+    #     # elem.click()
+    #     login_but = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.calendar-create-item.panel-item.panel-item__marked')))
+    #     self.driver.execute_script("arguments[0].click();", login_but)
+    #     element = self.driver.find_element_by_css_selector('.calendar-create-item.panel-item.panel-item__marked')
+    #     ActionChains(self.driver).move_to_element(element ).click(element ).perform()
+
+    def click_on_back_btn(self):
+        time.sleep(1)
+        elem = self.driver.find_element_by_css_selector('.header-menu-item__back')
+        time.sleep(1)
+        elem.click()
+
+    def click_on_ok_btn(self):
+        elem = self.driver.find_element_by_css_selector('.header-menu-item__ok')
+        elem.click()
+
+    def click_on_event(self):
+        # self.check_event_available()
+        # elem = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.panel-content:last-child')))
+        time.sleep(1)
+        elem = self.driver.find_element_by_css_selector('div.panel-content:last-child')
+        time.sleep(1)
+        elem.click()
+
+    def get_event(self):
+        # self.check_event_available()
+        # elem = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.panel-content:last-child')))
+        time.sleep(1)
+        elem = self.driver.find_element_by_css_selector('div.panel-item-text.brief-event-title')
+        time.sleep(1)
+        return elem
+
+    def get_count_of_events(self):
+        time.sleep(1)
+        # parent = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.panel-content')))
+        childs = [x.text for x in self.driver.find_elements_by_css_selector("div.panel-item.brief-event")]
+        # for c in childs:
+        #     print(c)
+        return len(childs)
+            
+    # def get_last_event_title(self):
+
+
+    def click_on_edit_btn(self):
+        time.sleep(1)
+        elem = self.driver.find_element_by_css_selector('.header-menu-item__edit')
+        time.sleep(1)
+        elem.click()
+        
+
+    def click_on_remove_btn(self):
+        time.sleep(1)
+        elem = self.driver.find_element_by_css_selector('.button-delete')
+        time.sleep(1)
+        elem.click()
 
     def enter_login(self, login):
         elem = self.driver.find_element_by_css_selector('input[name=Login]')
--- LoginTest.py
+++ LoginTest.py
@@ -7,53 +7,66 @@ from selenium import webdriver
 
 from LoginPage import LoginPage
 
+import time
+
 class LoginTest(unittest.TestCase):
+
+    LOGIN_URL = 'https://m.calendar.mail.ru/login'
+    BASE_URL = 'https://m.calendar.mail.ru/'
     login = os.environ.get('LOGIN')
     password = os.environ.get('PASSWORD')
 
     def setUp(self):
         self.driver = webdriver.Chrome('./chromedriver')
+        login_page = LoginPage(self.driver)
+        login_page.open(self.LOGIN_URL)
+        login_page.enter_login(self.login)
+        login_page.enter_password(self.password)
+        login_page.login()
+        login_page.wait_redirect(self.BASE_URL)
+
+    def test_create_event(self):
+        page = LoginPage(self.driver)
+        page.open(self.BASE_URL)
+        page.open_menu_plus()
+        count_of_childs_before = page.get_count_of_events()
+        page.enter_event_name('New event 1')
+        page.click_on_ok_btn()
+        page.wait_redirect('https://m.calendar.mail.ru/event/new/')
+        count_of_childs_after = page.get_count_of_events()
+        print('before: %d, after: %d'  % (count_of_childs_before, count_of_childs_after))
+        self.assertEqual(count_of_childs_before, count_of_childs_after - 1)
+
+
+    def test_jump_to_edit_event(self):
+        page = LoginPage(self.driver)
+        page.open(self.BASE_URL)
+        page.click_on_event()
+        page.click_on_edit_btn()
+        page.wait_redirect('https://m.calendar.mail.ru/event/new/')
+
+
+    def test_edit_event(self):
+        page = LoginPage(self.driver)
+        page.open(self.BASE_URL)
+        page.click_on_event()
+        page.click_on_edit_btn()
+        self.event_name = 'New event 3'
+        page.enter_event_name(self.event_name)
+        page.click_on_ok_btn()
+        page.click_on_back_btn()
+        event = page.get_event()
+        self.assertEqual(event.text, "New event 3")
+    
+    # def test_remove_event(self):
+    #     page = LoginPage(self.driver)
+    #     page.open(self.BASE_URL)
+    #     page.click_on_event()
+    #     page.click_on_remove_btn()
+    #     # Repeat to click on a remove button to confirm
+    #     page.click_on_remove_btn()
+    #     time.sleep(2)
+    #     self.assertEqual(page.get_count_of_events(), 0)
 
     def tearDown(self):
         self.driver.quit()
-
-    def test_correct_login(self):
-        login_page = LoginPage(self.driver)
-        login_page.open()
-        login_page.enter_login(self.login)
-        login_page.enter_password(self.password)
-        login_page.login()
-        login_page.wait_redirect('https://m.calendar.mail.ru/')
-
-    def test_incorrect_password(self):
-        wrong_password = 'wrongpassword'
-        login_page = LoginPage(self.driver)
-        login_page.open()
-        login_page.enter_login(self.login)
-        login_page.enter_password(wrong_password)
-        login_page.login()
-        login_page.wait_redirect('https://e.mail.ru/login')
-
-    def test_incorrect_email(self):
-        wrong_email = 'yandex.ru'
-        login_page = LoginPage(self.driver)
-        login_page.open()
-        login_page.enter_login(wrong_email)
-        login_page.enter_password(self.password)
-        login_page.login()
-        login_page.email_required()
-
-    def test_empty_fields(self):
-        login_page = LoginPage(self.driver)
-        login_page.login()
-        login_page.email_required()
-
-    def test_forgot_password(self):
-        login_page = LoginPage(self.driver)
-        login_page.forgot_password()
-        login_page.wait_redirect('https://account.mail.ru/')
-
-    def test_register(self):
-        login_page = LoginPage(self.driver)
-        login_page.register()
-        login_page.wait_redirect('https://account.mail.ru/')
