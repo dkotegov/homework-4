bfc134689f01b58f0d960a6834bb373d68c6f791
Dmitry <--global>
2019-12-17 03:33:49 +0300

    refactor add test

--- DealPage.py
+++ DealPage.py
@@ -4,6 +4,7 @@ import config
 
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
@@ -27,6 +28,27 @@ class DealPage:
     def login(self):
         elem = self.driver.find_element_by_css_selector('.login-button')
         elem.click()
+    
+    def enter_deal_name(self, name):
+        elem = self.driver.find_element_by_css_selector('.todo-new-summary-input')
+        elem.send_keys(name)
+    
+    def click_on_name_deal(self):
+        elem = self.driver.find_element_by_css_selector('.todo-new-main__disabled')
+        elem.click()
+
+    def click_on_today_btn(self):
+        elem = self.driver.find_element_by_css_selector('.todo-new-main')
+        elem.click()
+
+    def click_on_add_btn(self):
+        elem = self.driver.find_element_by_css_selector('.button-create')
+        elem.click()
+    
+    def checkDeal(self):
+        elem = self.driver.find_element_by_css_selector('.panel-item-content').text
+        return elem
+
 
     def wait_redirect(self, url, timeout = 15):
         return WebDriverWait(self.driver, timeout).until(EC.url_matches(url))
--- DealTest.py
+++ DealTest.py
@@ -20,13 +20,19 @@ class DealTest(unittest.TestCase):
         login_page.enter_password(self.password)
         login_page.login()
         login_page.wait_redirect(self.BASE_URL)
+        self.driver.refresh()
     
     def test_create_deal(self):
-        page = DealPage(self.driver)
-        page.open(self.BASE_URL)
-        
+        page = DealPage(self.driver)    
         page.wait_redirect('https://m.calendar.mail.ru/todos')
+        self.driver.refresh()
+        page.click_on_name_deal()
+        page.enter_deal_name('New deal')
+        page.click_on_add_btn()
         self.driver.refresh()
 
     def tearDown(self):
         self.driver.quit()
+
+
+        
