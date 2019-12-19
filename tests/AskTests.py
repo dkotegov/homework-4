55779a97faf97c8144560d721b5d8b4445f96206
Temirlan <panda13956@gmail.com>
2019-12-19 20:29:47 +0300

    fixes

--- tests/AskTests.py
+++ tests/AskTests.py
@@ -33,18 +33,17 @@ class AskTests(unittest.TestCase):
         self.driver.quit()
 
     def test_needThreeWords(self):
-        self.askPageOpen()
         self.page.clickLogin()
         self.page.login()
         self.page.open()
 
         self.page.setQuestionTheme('hello, world!')
         self.page.clickChooseAnother()
+        self.page.waitForAnother()
         self.page.clickSendQuestion()
         self.assertTrue(self.page.isAlert())
 
     def test_profile(self):
-        self.askPageOpen()
         self.page.clickLogin()
         self.page.login()
         self.page.open()
@@ -52,7 +51,6 @@ class AskTests(unittest.TestCase):
         self.assertTrue(self.page.clickAndWaitProfile())
 
     def test_notEmptyQuestion(self):
-        self.askPageOpen()
         self.page = AskPage(self.driver)
         self.page.open()
 
@@ -63,7 +61,6 @@ class AskTests(unittest.TestCase):
                          u'Поле «Тема вопроса» обязательно для заполнения')
 
     def test_mentionCountry(self):
-        self.askPageOpen()
         questionWithCountry = u'Россия'
         self.page.setQuestionTheme(questionWithCountry)
         self.page.autosettingSubcategory(u'Политика')
@@ -71,14 +68,12 @@ class AskTests(unittest.TestCase):
                          u'Политика')
 
     def test_loginBtn_and_authorization(self):
-        self.askPageOpen()
         self.page.clickLogin()
         self.assertTrue(self.page.lofinFormIsVisible())
         self.page.login()
         self.assertTrue(self.page.sameUrl("https://otvet.mail.ru"))
 
     def test_photoVideoUploadTest(self):
-        self.askPageOpen()
         self.page.open_photo_upload_form()
         self.page.can_press_esc()
 
@@ -86,16 +81,16 @@ class AskTests(unittest.TestCase):
         self.page.can_press_esc()
 
     def test_notValidTheme(self):
-        self.askPageOpen()
         self.page.clickLogin()
         self.page.login()
         self.page.open()
         self.page.setQuestionTheme(u'ыв ыва ыва 23')
+        self.page.clickChooseAnother()
+        self.page.waitForAnother()
         self.page.clickSendQuestion()
         self.assertFalse(self.page.isAlert())
 
     def test_tooBigQuestion(self):
-        self.askPageOpen()
         bigStr = u''
         for _ in range(122):
             bigStr = bigStr + u'a'
@@ -105,7 +100,6 @@ class AskTests(unittest.TestCase):
                          u'быть больше 120 символов.')
 
     def test_newQuestionEditTest(self):
-        self.askPageOpen()
         self.page.clickLogin()
         self.page.login()
         self.page.open()
@@ -124,7 +118,6 @@ class AskTests(unittest.TestCase):
         self.assertTrue(self.page.can_edit_time())
 
     def test_settingsTest(self):
-        self.askPageOpen()
         self.page.clickLogin()
         self.page.login()
         self.page.open()
