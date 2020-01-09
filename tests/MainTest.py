02d3310a0a202d1b1b7b31dd8b77291d909c83c5
Flyewzz <dentalon599@gmail.com>
2020-01-09 04:57:11 +0300

    tests unread_letter_status, reading/unreading letter have passed

--- tests/MainTest.py
+++ tests/MainTest.py
@@ -19,25 +19,25 @@ class MainTest(BasicTest):
     self.assertEqual(subject, actual_subject)
     self.assertEqual(text, actual_text)
     
-  # def test_unread_letter_status(self):
-  #   self.main_page.write_letter(self.login, 'Subject 2', 'Text 2')
-  #   self.assertFalse(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
+  def test_unread_letter_status(self):
+    self.main_page.letter_manager.write_letter(self.login, 'Subject 2', 'Text 2')
+    self.assertFalse(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
     
-  # def test_reading_letter(self):
-  #   self.main_page.write_letter(self.login, 'Subject 3', 'Text 3')
-  #   self.main_page.letter_selector.set_first_letter_read_status(True)
-  #   self.assertTrue(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
+  def test_reading_letter(self):
+    self.main_page.letter_manager.write_letter(self.login, 'Subject 3', 'Text 3')
+    self.main_page.letter_manager.letter_selector.set_first_letter_read_status(True)
+    self.assertTrue(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
     
-  # def test_unreading_letter(self):
-  #   self.main_page.write_letter(self.login, 'Subject4', 'Text4')
-  #   self.main_page.letter_manager.letter_selector.set_first_letter_read_status(True)
-  #   self.main_page.letter_manager.letter_selector.set_first_letter_read_status(False)
-  #   self.assertFalse(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
+  def test_unreading_letter(self):
+    self.main_page.letter_manager.write_letter(self.login, 'Subject4', 'Text4')
+    self.main_page.letter_manager.letter_selector.set_first_letter_read_status(True)
+    self.main_page.letter_manager.letter_selector.set_first_letter_read_status(False)
+    self.assertFalse(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
 
   # def test_remove_letter(self):
   #   subject = 'Subject 5'
   #   text = 'Text 5'
-  #   self.main_page.write_letter(self.login, subject, text)
+  #   self.main_page.letter_manager.write_letter(self.login, subject, text)
   #   self.main_page.remove_first_letter()
     
   #   self.main_page.click_nav_trash_button()
@@ -49,7 +49,7 @@ class MainTest(BasicTest):
   # def test_restore_letter(self):
   #   subject = 'Subject 6'
   #   text = 'Text 6'
-  #   self.main_page.write_letter(self.login, subject, text)
+  #   self.main_page.letter_manager.write_letter(self.login, subject, text)
   #   self.main_page.remove_first_letter()
     
   #   self.main_page.click_nav_trash_button()
@@ -63,13 +63,13 @@ class MainTest(BasicTest):
     
   # def test_remove_all_letters_from_inbox(self):
   #   for i in range(2):
-  #     self.main_page.write_letter(self.login, 'Subject7%d' % i, 'Text7%d' % i)
+  #     self.main_page.letter_manager.write_letter(self.login, 'Subject7%d' % i, 'Text7%d' % i)
   #   self.main_page.move_all_letters_to_trash()
   #   self.main_page.is_there_no_letters()
     
   # def test_restore_all_letters_to_inbox(self):
   #   for i in range(2):
-  #     self.main_page.write_letter(self.login, 'Subject__8%d' % i, 'Text__8%d' % i)
+  #     self.main_page.letter_manager.write_letter(self.login, 'Subject__8%d' % i, 'Text__8%d' % i)
       
   #   self.main_page.move_all_letters_to_trash()
   #   self.main_page.click_nav_trash_button()
@@ -78,7 +78,7 @@ class MainTest(BasicTest):
     
   # def test_clean_trash(self):
   #   for i in range(2):
-  #     self.main_page.write_letter(self.login, 'Subject #9%d' % i, 'Text #9%d' % i)
+  #     self.main_page.letter_manager.write_letter(self.login, 'Subject #9%d' % i, 'Text #9%d' % i)
       
   #   self.main_page.move_all_letters_to_trash()
   #   self.main_page.click_nav_trash_button()
@@ -88,7 +88,7 @@ class MainTest(BasicTest):
   # def test_check_sent_new_letter(self):
   #   subject = 'Subject 10'
   #   text = 'Text (10)'
-  #   self.main_page.write_letter(self.login, subject, text)
+  #   self.main_page.letter_manager.write_letter(self.login, subject, text)
   #   self.main_page.click_nav_sent_button()
   #   actual_subject = self.main_page.get_first_letter_subject()
   #   actual_text = self.main_page.get_first_letter_text()
