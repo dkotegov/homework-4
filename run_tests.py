d8ceacc0894527eae2eba60dd571b8eb2e98b8d1
Flyewzz <dentalon599@gmail.com>
2020-01-10 16:09:08 +0300

    Main is ready

--- run_tests.py
+++ run_tests.py
@@ -8,8 +8,6 @@ from tests.MainTest import MainTest
 if __name__ == '__main__':
     suite = unittest.TestSuite((
         unittest.makeSuite(MainTest),
-        # unittest.makeSuite(LoginTest),
-        # unittest.makeSuite(SettingsTest),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
