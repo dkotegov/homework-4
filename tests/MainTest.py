a55a9634d759996f4be9904640d29864be3c708f
Flyewzz <dentalon599@gmail.com>
2020-01-10 18:29:04 +0300

    Merge commit 'ef04972c22a191f50b48a318d5bb6b94caf0c443' into pre-master

--- tests/MainTest.py
+++ tests/MainTest.py
@@ -12,8 +12,8 @@ class MainTest(BasicTest):
     self.main_page.hide_app_loader()
     
   def test_receive_new_letter(self):
-    subject = 'Subject_1'
-    text = 'Text1'
+    subject = 'Subject_receive_new_letter'
+    text = 'Text_receive_new_letter'
     self.main_page.letter_manager.write_letter(self.login, subject, text)
     actual_subject = self.main_page.letter_manager.letter_selector.get_first_letter_subject()
     actual_text = self.main_page.letter_manager.letter_selector.get_first_letter_text()
@@ -21,23 +21,21 @@ class MainTest(BasicTest):
     self.assertEqual(text, actual_text)
     
   def test_unread_letter_status(self):
-    self.main_page.letter_manager.write_letter(self.login, 'Subject 2', 'Text 2')
+    subject = 'Subject_unread_letter_status'
+    text = 'Text_unread_letter_status'
+    self.main_page.letter_manager.write_letter(self.login, subject, text)
     self.assertFalse(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
     
   def test_reading_letter(self):
-    self.main_page.letter_manager.write_letter(self.login, 'Subject 3', 'Text 3')
+    subject = 'Subject_reading_letter'
+    text = 'Text_reading_letter'
+    self.main_page.letter_manager.write_letter(self.login, subject, text)
     self.main_page.letter_manager.letter_selector.set_first_letter_read_status(True)
     self.assertTrue(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
     
-  def test_unreading_letter(self):
-    self.main_page.letter_manager.write_letter(self.login, 'Subject4', 'Text4')
-    self.main_page.letter_manager.letter_selector.set_first_letter_read_status(True)
-    self.main_page.letter_manager.letter_selector.set_first_letter_read_status(False)
-    self.assertFalse(self.main_page.letter_manager.letter_selector.get_first_letter_read_status())
-
   def test_remove_letter(self):
-    subject = 'Subject 5'
-    text = 'Text 5'
+    subject = 'Subject_remove_letter'
+    text = 'Text_remove_letter'
     self.main_page.letter_manager.write_letter(self.login, subject, text)
     self.main_page.letter_manager.remove_first_letter()
     
@@ -48,8 +46,8 @@ class MainTest(BasicTest):
     self.assertEqual(text, actual_text)
     
   def test_restore_letter(self):
-    subject = 'Subject 6'
-    text = 'Text 6'
+    subject = 'Subject_restore_letter'
+    text = 'Text_restore_letter'
     self.main_page.letter_manager.write_letter(self.login, subject, text)
     self.main_page.letter_manager.remove_first_letter()
     
@@ -65,13 +63,17 @@ class MainTest(BasicTest):
     
   def test_remove_all_letters_from_inbox(self):
     for i in range(2):
-      self.main_page.letter_manager.write_letter(self.login, 'Subject7%d' % i, 'Text7%d' % i)
+      subject = 'Subject_remove_all_letters_from_inbox%d'
+      text = 'Text_remove_all_letters_from_inbox%d'
+      self.main_page.letter_manager.write_letter(self.login, subject % i, text % i)
     self.main_page.letter_manager.move_all_letters_to_trash()
     self.main_page.letter_manager.letter_selector.is_there_no_letters()
     
   def test_restore_all_letters_to_inbox(self):
     for i in range(2):
-      self.main_page.letter_manager.write_letter(self.login, 'Subject__8%d' % i, 'Text__8%d' % i)
+      subject = 'Subject_restore_all_letters_to_inbox%d'
+      text = 'Text_restore_all_letters_to_inbox%d'
+      self.main_page.letter_manager.write_letter(self.login, subject % i, text % i)
       
     self.main_page.letter_manager.move_all_letters_to_trash()
     self.main_page.navigation_manager.go_to_trash()
@@ -80,7 +82,9 @@ class MainTest(BasicTest):
     
   def test_clean_trash(self):
     for i in range(2):
-      self.main_page.letter_manager.write_letter(self.login, 'Subject #9%d' % i, 'Text #9%d' % i)
+      subject = 'Subject_clean_trash%d'
+      text = 'Text_clean_trash%d'
+      self.main_page.letter_manager.write_letter(self.login, subject % i, text % i)
       
     self.main_page.letter_manager.move_all_letters_to_trash()
     self.main_page.navigation_manager.go_to_trash()
@@ -88,11 +92,15 @@ class MainTest(BasicTest):
     self.main_page.letter_manager.letter_selector.is_there_no_letters()
     
   def test_check_sent_new_letter(self):
-    subject = 'Subject 10'
-    text = 'Text (10)'
+    subject = 'Subject_check_sent_new_letter'
+    text = 'Text_check_sent_new_letter'
     self.main_page.letter_manager.write_letter(self.login, subject, text)
     self.main_page.navigation_manager.go_to_sent_letters_folder()
     actual_subject = self.main_page.letter_manager.letter_selector.get_first_letter_subject()
     actual_text = self.main_page.letter_manager.letter_selector.get_first_letter_text()
     self.assertEqual(subject, actual_subject)
     self.assertEqual(text, actual_text)
+    
+    
+    
+    
