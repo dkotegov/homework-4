b5ea9009962d75bc771df0407cfa143265ac54b6
DmitriyPrischep <dmitriygoogle@yandex.ru>
2020-01-11 18:59:15 +0300

    feat: added any tests for folders

--- tests/pages/DirectoryPage.py
+++ tests/pages/DirectoryPage.py
@@ -3,17 +3,31 @@ from BasicPage import BasicPage
 from MainPage import MainPage
 from selenium.webdriver import ActionChains
 from tests.pages.main_page.notifications.NotificationManager import NotificationManager
+from tests.pages.main_page.menu.top_menu import TopMenuManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 class DirectoryPage(MainPage):
     ARCHIVE_URL = 'https://e.mail.ru/archive'
+    DRAFTS_URL = 'https://e.mail.ru/drafts'
+    SOCIAL_URL = 'https://e.mail.ru/social'
+    NEWS_LETTERS_URL = 'https://e.mail.ru/newsletters'
     click_flag = '.ll-fs'
+    top_menu_move = 'div.portal-menu-element_move'
     click_flag_activate = ".ll-fs_is-active"
     flag_activate_class = "ll-fs ll-fs_is-active"
     archive_button = "div.portal-menu-element_archive"
+    social_button = "div.list-item[title='Социальные сети']"
+    news_letter_button = "div.list-item[title='Рассылки']"
+    drafts_button = "div.portal-menu-element_drafts"
     nav_archive_button = "a.nav__item[title='Архив']"
+    nav_drafts_button = "a.nav__item[title='Черновики']"
+    # nav_social_button = "a.nav__item[title='1 письмо, 1 непрочитанное']"
+    # nav_social_button = "a.nav__item_child.nav__item_expanded_true[href='/social/']"
+    # nav_social_button = "a.nav__item_child:nth-of-type(1)"
+    nav_newsletters_button = "a.nav__item[title='Рассылки']"
+    nav_all_folders_button = "a.sidebar__menu-item"
     select_letter_button = 'a.llc:first-of-type .ll-rs'
 
 
@@ -24,6 +38,23 @@ class DirectoryPage(MainPage):
     def open(self):
         self.driver.get(self.LOGIN_URL)
 
+    def move_to_social(self):
+        elem = self.wait_render(self.top_menu_move)
+        ActionChains(self.driver).move_to_element(elem).click(elem).perform()
+        
+        elem = self.wait_render(self.social_button)
+        ActionChains(self.driver).move_to_element(elem).click(elem).perform()
+        self.notification_manager.hide_notification()
+
+    def move_to_newsletters(self):
+        elem = self.wait_render(self.top_menu_move)
+        ActionChains(self.driver).move_to_element(elem).click(elem).perform()
+
+        elem = self.wait_render(self.news_letter_button)
+        ActionChains(self.driver).move_to_element(elem).click(elem).perform()
+        self.notification_manager.hide_notification()
+
+    
     def move_to_archive(self):
         elem = self.wait_render(self.archive_button)
         elem.click()
@@ -33,6 +64,21 @@ class DirectoryPage(MainPage):
         elem = self.wait_render(self.nav_archive_button)
         elem.click()
         self.wait_redirect(self.ARCHIVE_URL)
+    
+    def go_to_newsletters(self):
+        elem = self.wait_render(self.nav_all_folders_button)
+        elem.click()
+        # elem = self.wait_render(self.nav_newsletters_button)
+        self.driver.get(self.NEWS_LETTERS_URL)
+        self.wait_redirect(self.NEWS_LETTERS_URL)
+    
+    def go_to_social(self):
+        elem = self.wait_render(self.nav_all_folders_button)
+        elem.click()
+        # elem = self.wait_render(self.nav_social_button)
+        # ActionChains(self.driver).move_to_element(elem).click(elem).perform()
+        self.driver.get(self.SOCIAL_URL)
+        self.wait_redirect(self.SOCIAL_URL)
         
     def select_letter(self):
         elem = self.wait_render(self.select_letter_button)
