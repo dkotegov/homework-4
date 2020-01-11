bf2d07d2bf645289476e718cce8dccc842819a24
Vlad104 <vlad104khk@gmail.com>
2020-01-11 15:39:09 +0500

    feat: add test

--- tests/SettingsTest.py
+++ tests/SettingsTest.py
@@ -9,7 +9,7 @@ class SettingsTest(BasicTest):
     AUTH_URL = 'https://e.mail.ru/login'
     SETTINGS_URL = 'https://e.mail.ru/settings'
     SETTINGS_OK_URL = 'https://e.mail.ru/settings?result=ok&afterReload=1'
-    SETTINGS_NOTIFICATIONS_URL = 'https://e.mail.ru/settings/notifications'
+    SETTINGS_MESSAGES_URL = 'https://e.mail.ru/settings/messages'
 
     def setUp(self):
         super(SettingsTest, self).setUp()
@@ -30,6 +30,14 @@ class SettingsTest(BasicTest):
         self.settings_page.wait_redirect(self.SETTINGS_URL)
         self.assertEqual(self.driver.current_url, self.SETTINGS_OK_URL)
 
+    def test_save_fields(self):
+        self.settings_page.enter_firstname('Another new name')
+        self.settings_page.enter_lastname('New last name')
+        self.settings_page.enter_nickname('New nick name')
+        self.settings_page.save()
+        self.settings_page.wait_redirect(self.SETTINGS_URL)
+        self.assertEqual(self.driver.current_url, self.SETTINGS_OK_URL)
+
     def test_save_empty_field(self):
         self.settings_page.enter_firstname('')
         self.settings_page.save()
@@ -44,8 +52,9 @@ class SettingsTest(BasicTest):
         self.settings_page.wait_redirect(self.SETTINGS_URL)
         self.assertEqual(self.driver.current_url, self.SETTINGS_OK_URL)
 
-    def test_switch_notifications(self):
-        self.settings_page.notification_settings()
-        self.settings_page.wait_redirect(self.SETTINGS_NOTIFICATIONS_URL)
-        self.settings_page.notification_change_mode()
-        self.settings_page.wait_render(self.settings_page.save_message)
+    def test_switch_mailling(self):
+        self.settings_page.mailling_settings()
+        self.settings_page.wait_redirect(self.SETTINGS_MESSAGES_URL)
+        self.settings_page.mailling_switch()
+        self.settings_page.save_mailling()
+        self.assertEqual(self.driver.current_url, self.SETTINGS_OK_URL)
--- tests/pages/SettingsPage.py
+++ tests/pages/SettingsPage.py
@@ -5,14 +5,16 @@ from selenium.webdriver import ActionChains
 
 class SettingsPage(BasicPage):
     firstname = '#FirstName'
+    lastname = '#LastName'
+    nickname = '#NickName'
     city = '#your_town'
-    city_suggest = 'div.form__field__suggest:nth-child(1) > span:nth-child(1) > div:nth-child(1)'
+    city_suggest = '#formPersonal > div:nth-child(19) > div.form__row__widget > div:nth-child(2) > div'
     save_button = '#formPersonal > div.form__actions__wrapper > div > div > button'
     error_message = 'div.form__top-message.form__top-message_error'
     error_field_message = '#formPersonal > div:nth-child(12) > div.form__row__widget > div'
-    notification_tab = '#LEGO > div.b-layout.b-layout_main > div.b-layout__col.b-layout__col_1_2 > div > div:nth-child(9) > a > span'
-    notification_checkbox = 'div.c2165'
-    save_message = 'div > div.notify__content > span.notify__message'
+    mailling_tab = '#LEGO > div.b-layout.b-layout_main > div.b-layout__col.b-layout__col_1_2 > div > div:nth-child(3) > a > span'
+    mailling_checkbox = '#send_ads'
+    save_mailling_button = '#formMail > div.form__actions__wrapper > div > div > button'
 
     def __init__(self, driver):
         self.driver = driver
@@ -20,10 +22,20 @@ class SettingsPage(BasicPage):
     def open(self):
         self.driver.get(self.SETTINGS_URL)
 
-    def enter_firstname(self, login):
+    def enter_firstname(self, text):
         elem = self.wait_render(self.firstname)
         elem.clear()
-        elem.send_keys(login)
+        elem.send_keys(text)
+
+    def enter_lastname(self, text):
+        elem = self.wait_render(self.lastname)
+        elem.clear()
+        elem.send_keys(text)
+
+    def enter_nickname(self, text):
+        elem = self.wait_render(self.nickname)
+        elem.clear()
+        elem.send_keys(text)
 
     def enter_city(self, city):
         elem = self.wait_render(self.city)
@@ -40,10 +52,14 @@ class SettingsPage(BasicPage):
         elem = self.wait_render(self.save_button)
         elem.click()
 
-    def notification_settings(self):
-        elem = self.wait_render(self.notification_tab)
+    def mailling_settings(self):
+        elem = self.wait_render(self.mailling_tab)
         elem.click()
 
-    def notification_change_mode(self):
-        elem = self.wait_render(self.notification_checkbox)
-        ActionChains(self.driver).move_to_element(elem).click(elem).perform()
+    def mailling_switch(self):
+        elem = self.wait_render(self.mailling_checkbox)
+        elem.click()
+
+    def save_mailling(self):
+        elem = self.wait_render(self.save_mailling_button)
+        elem.click()
