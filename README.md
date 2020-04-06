8c47464293ad4bfc6dea01ab7d62cd0ff08fee46
Artyom Bakulev <i@artbakulev.com>
2020-04-06 10:48:33 +0300

    setup

---
+++ README.md
@@ -0,0 +1,16 @@
+### Обеспечение качества | Домашнее задание №2
+
+### Подготовка:
+#### Запуск ноды
+mac: `./launchers/run-node-mac.sh` 
+linux: `./launchers/run-node-linux.sh`
+
+#### Запуск хаба
+`./launchers/hub.sh` 
+
+#### Запуск тестов:
+`python run_tests.py`
+
+#### Сервис для тестирования
+
+[cinsear.ru](https://cinsear.ru)
---
+++ scripts/run-node-mac.sh
@@ -0,0 +1,7 @@
+sudo java -D webdriver.chrome.driver="./drivers/mac/chromedriver" \
+    -D webdriver.gecko.driver="./drivers/mac/geckodriver" \
+    -jar drivers/selenium-server-standalone-3.141.59.jar \
+    -role node \
+    -hub http://127.0.0.1:4444/grid/register \
+    -browser browserName=chrome,maxInstances=2 \
+    -browser browserName=firefox,maxInstances=2
