fefafd72831d260c6664f5716f708bfd3a7fe1c8
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2020-04-30 20:23:29 +0300

    Add test for auth

--- tests/cases/auth/test_auth.py
+++ tests/cases/auth/test_auth.py
@@ -2,9 +2,36 @@ from tests.steps.auth.steps import Steps
 
 
 class TestLogin:
+    INCORRECT_LOGIN = 'aaaa@'
+    INCORRECT_PASSWORD = '1'
+
     def test_success_login(self):
         Steps.open_site()
         Steps.open_auth_page()
         Steps.enter_credentials()
         Steps.login()
 
+    def test_incorrect_login_correct_password(self):
+        Steps.open_site()
+        Steps.open_auth_page()
+        Steps.enter_incorrect_login(self.INCORRECT_LOGIN)
+        Steps.enter_correct_password()
+        Steps.submit_login()
+        Steps.find_login_login_error()
+
+    def test_correct_login_incorrect_password(self):
+        Steps.open_site()
+        Steps.open_auth_page()
+        Steps.enter_correct_login()
+        Steps.enter_incorrect_password(self.INCORRECT_PASSWORD)
+        Steps.submit_login()
+        Steps.find_login_password_error()
+
+    def test_incorrect_login_incorrect_password(self):
+        Steps.open_site()
+        Steps.open_auth_page()
+        Steps.enter_incorrect_login(self.INCORRECT_LOGIN)
+        Steps.enter_incorrect_password(self.INCORRECT_PASSWORD)
+        Steps.submit_login()
+        Steps.find_login_login_error()
+        Steps.find_login_password_error()
--- tests/steps/auth/steps.py
+++ tests/steps/auth/steps.py
@@ -19,33 +19,68 @@ class Steps(BaseSteps):
         Pages.click_login_button()
         Pages.wait_until_page_load()
 
+    @staticmethod
     def auth():
         Steps.open_site()
         Steps.open_auth_page()
         Steps.enter_credentials()
         Steps.login()
 
+    @staticmethod
     def enter_register(name, login, pass1, pass2):
-
         Pages.set_register_name(name)
         Pages.set_register_email(login)
         Pages.set_register_password(pass1)
         Pages.set_register_password_clone(pass2)
 
+    @staticmethod
     def submit_reqister():
         Pages.click_reqister_button()
-    
-    def find_register_name_error(): 
+
+    @staticmethod
+    def find_register_name_error():
         Pages.find_register_name_error()
 
+    @staticmethod
     def find_register_login_error():
         Pages.find_register_email_error()
 
-    def find_register_password_error(): 
+    @staticmethod
+    def find_register_password_error():
         Pages.find_register_password_error()
 
+    @staticmethod
     def find_register_password_clone_error():
         Pages.find_register_password_clone_error()
 
+    @staticmethod
     def find_register_error():
         Pages.find_register_error()
+
+    @staticmethod
+    def enter_incorrect_login(login):
+        Pages.enter_login_login(login)
+
+    @staticmethod
+    def enter_correct_password():
+        Pages.enter_password()
+
+    @staticmethod
+    def submit_login():
+        Pages.click_login_button()
+
+    @staticmethod
+    def find_login_login_error():
+        Pages.find_login_login_error()
+
+    @staticmethod
+    def find_login_password_error():
+        Pages.find_login_password_error()
+
+    @staticmethod
+    def enter_correct_login():
+        Pages.enter_username()
+
+    @staticmethod
+    def enter_incorrect_password(password):
+        Pages.enter_login_password(password)
