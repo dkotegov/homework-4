3e2921c523856f65871be370e4ad92737ea5e697
Artyom Bakulev <i@artbakulev.com>
2020-04-30 23:23:53 +0300

    hello from artyom

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,19 @@
 import subprocess
+import os
+from time import sleep
 
 import pytest
 
+LINUX = 'linux'
+MAC = 'mac'
+
 if __name__ == '__main__':
     subprocess.call("./scripts/run-hub.sh&", shell=True)
-    subprocess.call("./scripts/run-node-linux.sh&", shell=True)
+    sleep(5)
+    operation_system = os.environ.get('os')
+    if operation_system == LINUX:
+        subprocess.call("./scripts/run-node-mac.sh &", shell=True)
+    else:
+        subprocess.call("./scripts/run-node-mac.sh &", shell=True)
+    sleep(5)
     pytest.main()
