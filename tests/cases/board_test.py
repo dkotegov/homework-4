74f6b630939cbe42ab318bb3145781bbdb32c799
BarniBl <VeinskiyVA@mail.ru>
2020-05-01 16:26:09 +0300

    fixed output message, added change board on edit pin

--- tests/cases/board_test.py
+++ tests/cases/board_test.py
@@ -1,10 +1,3 @@
-import os
-import time
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import DesiredCapabilities, Remote
-
 from tests.cases.base import TestAuthorized
 from tests.pages.create_board import CreateBoardPage
 
@@ -14,9 +7,9 @@ class Test(TestAuthorized):
         super().setUp()
         self.page = CreateBoardPage(self.driver)
 
-    def test_valid_data(self):
-        board_name = "this is valid test board"
-        board_content = "this is normal board description"
+    def test_create_board_valid_data(self):
+        board_name = "test_create_board_valid_data name"
+        board_content = "test_create_board_valid_data description"
         self.page.form_list.set_board_name(board_name)
         self.page.form_list.set_board_content(board_content)
         self.page.form_list.create_board()
@@ -27,18 +20,18 @@ class Test(TestAuthorized):
                 return
         assert "board not found"
 
-    def test_empty_name_description(self):
+    def test_create_board_empty_name_description(self):
         self.page.form_list.create_board()
         assert self.page.form_list.get_error() == ''
 
-    def test_empty_name(self):
-        board_content = "this is normal board description"
+    def test_create_board_empty_name(self):
+        board_content = "test_create_board_empty_name description"
         self.page.form_list.set_board_content(board_content)
         self.page.form_list.create_board()
         assert self.page.form_list.get_error() == ''
 
-    def test_empty_description(self):
-        board_name = "this is empty name board"
+    def test_create_board_empty_description(self):
+        board_name = "test_create_board_empty_description name"
         self.page.form_list.set_board_name(board_name)
         self.page.form_list.create_board()
         self.page.form_concrete.wait_for_load()
@@ -48,5 +41,5 @@ class Test(TestAuthorized):
                 return
         assert "board not found"
 
-    def test_go_back(self):
+    def test_create_board_go_back(self):
         self.page.form_list.go_back()
--- tests/cases/pin_test.py
+++ tests/cases/pin_test.py
@@ -1,10 +1,5 @@
 import os
 import random
-import time
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import DesiredCapabilities, Remote
 
 from tests.cases.base import TestAuthorized
 from tests.pages.create_pin import CreatePinPage
@@ -32,19 +27,18 @@ class Test(TestAuthorized):
                 break
         self.page = CreatePinPage(self.driver)
 
-    def test_valid_data(self):
-        pin_name = "this is valid test pin name"
-        pin_content = "this is normal pin description"
-        file_name = self.file_path
+    def test_create_pin_valid_data(self):
+        pin_name = "test_create_pin_valid_data name"
+        pin_content = "test_create_pin_valid_data description"
         self.page.form_list.set_pin_name(pin_name)
         self.page.form_list.set_pin_content(pin_content)
-        self.page.form_list.load_file(file_name)
+        self.page.form_list.load_file(self.file_path)
         self.page.form_list.set_select_board(self.board_id)
         self.page.form_list.create_pin()
         if self.page.form_list.get_error() != '':
             assert "error"
 
-    def test_empty_file(self):
+    def test_create_pin_empty_file(self):
         pin_name = "this is empty file test pin name"
         pin_content = "this is normal pin description"
         self.page.form_list.set_pin_name(pin_name)
@@ -53,22 +47,24 @@ class Test(TestAuthorized):
         self.page.form_list.create_pin()
         assert self.page.form_list.get_error() == ''
 
-    def test_empty_name(self):
-        file_name = self.file_path
+    def test_create_pin_empty_name(self):
         pin_content = "this is normal pin description"
         self.page.form_list.set_pin_content(pin_content)
-        self.page.form_list.load_file(file_name)
+        self.page.form_list.load_file(self.file_path)
         self.page.form_list.set_select_board(self.board_id)
         self.page.form_list.create_pin()
         assert self.page.form_list.get_error() == ''
 
-    def test_empty_description(self):
+    def test_create_pin_empty_description(self):
         pin_name = "this is empty description test pin name"
-        file_name = self.file_path
         self.page.form_list.set_pin_name(pin_name)
-        self.page.form_list.load_file(file_name)
+        self.page.form_list.load_file(self.file_path)
         self.page.form_list.set_select_board(self.board_id)
         self.page.form_list.create_pin()
 
-    def test_go_back(self):
+    def test_create_pin_empty(self):
+        self.page.form_list.create_pin()
+        assert self.page.form_list.get_error() == ''
+
+    def test_create_pin_go_back(self):
         self.page.form_list.go_back()
