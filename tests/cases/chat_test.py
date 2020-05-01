9d97087bbba3970f1bfdb4176202f391d45f44b2
Andrey Kochnov <a.kochnov@corp.mail.ru>
2020-05-01 17:57:13 +0300

    вычистили тесты от ui-хлама, поправки по ожиданиям

--- tests/cases/chat_test.py
+++ tests/cases/chat_test.py
@@ -7,64 +7,66 @@ class Test(TestAuthorized):
         super().setUp()
         self.page = ChatPage(self.driver)
 
-    def _send_message_positive(self, name, text, **kwargs):
+    def _send_message(self, name, text, **kwargs):
         self.page.form_list.search_user(name)
-        assert not self.page.form_list.get_error()
+        self.assertFalse(self.page.form_list.get_error(), "Got unexpected error")
 
-        self.page.form_concrete.wait_for_load()
-        assert self.page.form_concrete.get_companion_name() == name
-        self.page.form_concrete.send_message_confirmed(text, **kwargs)
+        self.assertEqual(self.page.form_concrete.get_companion_name(), name, "Wrong companion name")
+        return self.page.form_concrete.send_message_confirmed(text, **kwargs)
 
     def test_send_plain_to_existed(self):
         name, text = "testTest", "43424342"
-        self._send_message_positive(name, text)
+        from_me, got_text = self._send_message(name, text)
+        self.assertTrue(from_me, "Message not from me")
+        self.assertEqual(got_text, text, "Got wrong message")
 
     def test_search_non_existent(self):
         name = "odjwedjfdjefce"
         self.page.form_list.search_user(name)
         err = self.page.form_list.get_error(timeout=5)
-        assert err != ''
+        self.assertNotEqual(err, '')
 
     def test_forgot_to_print_username(self):
         name = ""
         self.page.form_list.search_user(name)
         err = self.page.form_list.get_error(timeout=5)
-        assert err != ''
+        self.assertNotEqual(err, '')
 
     def test_send_empty_message(self):
         name = "testTest"
-        self.page.form_list.search_user(name)
-        assert not self.page.form_list.get_error()
-
-        self.page.form_concrete.wait_for_load()
-        assert self.page.form_concrete.get_companion_name() == name
-        text1 = ""
+        text = ""
         try:
-            self.page.form_concrete.send_message_confirmed(text1, timeout=3)
+            self._send_message(name, text, timeout=3)
+            self.assertTrue(False, "There must not any message-list alteration be found, but smt has changed")
         except TimeoutError:
             return
-        assert False, "There must not any message-list alteration be found, but smt has changed"
 
     def test_send_smiles_only(self):
         name, text = "testTest", "😁😅😊😋😎"
-        self._send_message_positive(name, text,
-                                    printer=self.page.form_concrete.enter_smiles)
+        from_me, got_text = self._send_message(name, text,
+                                               printer=self.page.form_concrete.enter_smiles)
+        self.assertTrue(from_me, "Got message not from me")
+        self.assertEqual(got_text, text, "Got wrong message")
 
     def test_send_smiles_and_text(self):
         name, text = "testTest", "😁😅😊kokoko😋😎"
-        self._send_message_positive(name, text,
-                                    printer=self.page.form_concrete.enter_smiles)
+        from_me, got_text = self._send_message(name, text,
+                                               printer=self.page.form_concrete.enter_smiles)
+        self.assertTrue(from_me, "Got message not from me")
+        self.assertEqual(got_text, text, "Got wrong message")
 
     def test_open_saved_dialog(self):
-        name = 'testTest'
+        name = 'ForTest'
+        self._send_message(name, "setup")
+        self.page.open()
         self.page.form_list.open_dialog_from_list(name)
         self.page.form_concrete.wait_for_load()
         companion = self.page.form_concrete.get_companion_name()
-        assert companion == name
+        self.assertEqual(companion, name, "Wrong companion name")
 
     def test_send_message_check_update_chats_list(self):
         name, text = 'testTest', 'fkererfrfe'
-        self._send_message_positive(name, text)
+        self._send_message(name, text)
         self.driver.refresh()
         self.page.form_list.wait_for_load()
         self.page.form_list.check_dialog(name, text)
