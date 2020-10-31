81ab5c604f08fc453508ffe7e067b73c1967673b
Oleg <elizarov1999@list.ru>
2020-10-31 15:58:07 +0300

    Add example search test

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 if __name__ == '__main__':
+    browser = webdriver.Chrome('./chromedriver')
+    browser.get("https://otvet.mail.ru")
+    print(browser.title)
+    search = browser.find_element_by_css_selector("[bem-id='182']")
+    search.send_keys("TEXT")
+
+    button = browser.find_element_by_css_selector("[bem-id='185']")
+    button.click()
+
+    WebDriverWait(browser, 5)
     pass
