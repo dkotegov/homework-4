b861cdb48a925fa3c1208b9f7f5137b53b777e6c
Oleg <elizarov1999@list.ru>
2020-11-01 18:32:41 +0300

    Add base test from main cases

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,9 +1,14 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import re
+
+EMPTY_TEXT = "Задан пустой\nпоисковый запрос"
+EMPTY_RES = "По вашему запросу\nничего не найдено"
 
 
 class SearchPage(unittest.TestCase):
@@ -22,21 +27,70 @@ class SearchPage(unittest.TestCase):
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "dataset-letters")))
         print("LOGIN")
 
-    def test_nav_search_input(self):
+    # def send_query(self, text):
+    #     browser = self.browser
+    #     input = browser.find_element_by_css_selector("[data-qa='input']")
+    #     input.send_keys(text)
+    #     input.send_keys(Keys.ENTER)
+
+    def test_nav_search_input(self,text):
         self.browser.get("https://otvet.mail.ru")
         browser = self.browser
-        print(browser.title)
-
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, self.nav_search)))
         search = browser.find_element_by_css_selector(self.nav_search)
-        search.send_keys("TEXT")
-
+        search.send_keys(text)
         button = browser.find_element_by_css_selector(self.nav_search_button)
         button.click()
-
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "search_value")))
         nav_text = browser.find_element_by_css_selector(self.nav_search).get_attribute('value')
         str_text = browser.find_element_by_name("search_value").get_attribute('value')
-        # str_text = browser.find_element_by_name("search_value").get_attribute('value')
-        print(nav_text, " ! ", str_text)
         self.assertEqual(nav_text, str_text)
+
+    def test_empty_query(self, text):
+        self.browser.get("https://otvet.mail.ru/search")
+        browser = self.browser
+        input = browser.find_element_by_css_selector("[data-qa='input']")
+        input.send_keys(text)
+        input.send_keys(Keys.ENTER)
+        input.send_keys(text)
+        # Deleting hear
+        # input.clear()
+        input.send_keys(Keys.COMMAND, 'a')
+        input.send_keys(Keys.BACKSPACE)
+        input.send_keys(Keys.ENTER)
+        WebDriverWait(browser, 20)
+        empty_text = browser.find_element_by_css_selector("[class^='noResultsTitle']").text
+        self.assertEqual(empty_text, EMPTY_TEXT)
+
+    def test_special_symb(self, text):
+        self.browser.get("https://otvet.mail.ru/search")
+        browser = self.browser
+        input = browser.find_element_by_css_selector("[data-qa='input']")
+        input.send_keys(text)
+        input.send_keys(Keys.ENTER)
+        input.send_keys(text)
+
+        input.send_keys(Keys.COMMAND, 'a')
+        input.send_keys(Keys.BACKSPACE)
+        input.send_keys("!@#$%^&*()")
+        input.send_keys(Keys.ENTER)
+        WebDriverWait(browser, 20)
+        empty_res = browser.find_element_by_css_selector("[class^='noResultsTitle']").text
+        self.assertEqual(empty_res, EMPTY_RES)
+
+    def test_question_only_button(self, text):
+        self.browser.get("https://otvet.mail.ru/search")
+        browser = self.browser
+        input = browser.find_element_by_css_selector("[data-qa='input']")
+        input.send_keys(text)
+        input.send_keys(Keys.ENTER)
+        WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[class^='question__link_holder']")))
+        total_count = browser.find_element_by_css_selector("[class^='totalResults']").text.replace(" ", "")
+        browser.find_element_by_css_selector("[class^='checkbox']").click()
+        input.send_keys(Keys.ENTER)
+        WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[class^='question__link_holder']")))
+        only_quest_count = browser.find_element_by_css_selector("[class^='totalResults']").text.replace(" ", "")
+        self.assertGreaterEqual(
+            re.findall(r'\d+', total_count)[0], re.findall(r'\d+', only_quest_count)[0])
--- test_suites/search_test.py
+++ test_suites/search_test.py
@@ -5,7 +5,22 @@ from pages.search_page import SearchPage as page
 
 class SearchTests(unittest.TestCase):
 
-    def test_nav_search_input(self):
+    def test_asetup(self):
         self.page = page()
         self.page.login()
-        self.page.test_nav_search_input()
+
+    def test_nav_search_input(self):
+        self.page = page()
+        self.page.test_nav_search_input("TEXT")
+
+    def test_empty_query(self):
+        self.page = page()
+        self.page.test_empty_query("TEXT")
+
+    def test_special_query(self):
+        self.page = page()
+        self.page.test_special_symb("TEXT")
+
+    def test_only_quest_filter(self):
+        self.page = page()
+        self.page.test_question_only_button("TEXT")
