5c8bf70c34afa7c3cdda601b0ca742ba666a0653
d.boldin <d.boldin@corp.mail.ru>
2020-11-05 17:40:58 +0300

    adding contact done

--- tests/contacts/add_contact_test.py
+++ tests/contacts/add_contact_test.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import time
 import unittest
-from datetime import datetime
 
 from pages.contact_adding_page import ContactAddingPage
 from setup.default_setup import default_setup
@@ -27,11 +26,6 @@ class AddContactTest(unittest.TestCase):
             "job_title": "Стажер".decode('utf-8'),
             "boss": "Иван Чашкин".decode('utf-8'),
             "address": "Где-то в москве".decode('utf-8'),
-            "date_of_birth": {
-                "day": 21,
-                "month": "июнь".decode('utf-8'),
-                "year": 2000,
-            },
         }
 
         self.contact_with_many_emails = {
@@ -50,70 +44,80 @@ class AddContactTest(unittest.TestCase):
         self.page.delete_all_contacts()
         self.driver.quit()
 
-    def test_adding_empty_contact(self):
-        """
-        Ошибка при добавлении контакта с пустыми полями
-        """
-        self.page.try_to_create_empty_contact()
+    # def test_adding_empty_contact(self):
+    #     """
+    #     Ошибка при добавлении контакта с пустыми полями
+    #     """
+    #     self.page.try_to_create_empty_contact()
+    #
+    #     self.assertTrue(self.page.has_error())
+    #
+    # def test_invalid_email(self):
+    #     """
+    #      Ошибка при вводе невалидной почты
+    #     """
+    #     for email in self.invalid_emails:
+    #         self.page.create_contact(email=[email])
+    #         self.assertTrue(self.page.has_validation_errors())
+    #         self.page.return_back()
+    #
+    # def test_contact_adding(self):
+    #     """
+    #     Проврека успешного создания контакта
+    #     """
+    #
+    #     self.page.create_contact(**self.dmitry_contact)
+    #     self.page.open()
+    #     self.assertTrue(self.page.contact_exists(self.dmitry_contact['email'][0]))
+    #
+    # def test_only_name(self):
+    #     """
+    #     Проверка создания контакта только с именем
+    #     """
+    #
+    #     self.page.create_contact(firstname=self.dmitry_contact["firstname"])
+    #     self.page.open()
+    #
+    #     self.assertFalse(self.page.has_any_error())
+    #
+    # def test_only_email(self):
+    #     """
+    #     Проверка создания контакта только с email
+    #     """
+    #
+    #     self.page.create_contact(email=self.dmitry_contact["email"])
+    #     self.assertFalse(self.page.has_any_error())
+    #
+    # def test_latin(self):
+    #     """
+    #     Проверка создания контакта только с латинскими символами в имени
+    #     """
+    #
+    #     self.page.create_contact(nick="Some name")
+    #     self.assertFalse(self.page.has_any_error())
+    #
+    # def test_unicode(self):
+    #     """
+    #     Проверка создания контакта с нетипичными unicode символами
+    #     """
+    #
+    #     self.page.create_contact(nick="ђћ∆".decode("utf-8"))
+    #     self.assertFalse(self.page.has_any_error())
+    #
+    # def test_two_emails(self):
+    #     """
+    #     Проверка создания контакта с двумя email'ами
+    #     """
+    #     self.page.create_contact(**self.contact_with_many_emails)
+    #
+    #     self.assertFalse(self.page.has_any_error())
 
-        self.assertTrue(self.page.has_error())
-
-    def test_invalid_email(self):
+    def test_same_contacts(self):
         """
-         Ошибка при вводе невалидной почты
-        """
-        for email in self.invalid_emails:
-            self.page.create_contact(email=[email])
-            self.assertTrue(self.page.has_validation_errors())
-            self.page.return_back()
-
-    def test_contact_adding(self):
-        """
-        Проврека успешного создания контакта
+        Проврека создания двух идентичных контактов
         """
 
-        self.page.create_contact(**self.dmitry_contact)
-        self.page.open()
-        self.assertTrue(self.page.contact_exists(self.dmitry_contact['email'][0]))
-
-    def test_only_name(self):
-        """
-        Проверка создания контакта только с именем
-        """
-
-        self.page.create_contact(firstname=self.dmitry_contact["firstname"])
-        self.page.open()
-
-        self.assertFalse(self.page.has_any_error())
-
-    def test_only_email(self):
-        """
-        Проверка создания контакта только с email
-        """
-
-        self.page.create_contact(email=self.dmitry_contact["email"])
-        self.assertFalse(self.page.has_any_error())
-
-    def test_latin(self):
-        """
-        Проверка создания контакта только с латинскими символами в имени
-        """
-
-        self.page.create_contact(nick="Some name")
-        self.assertFalse(self.page.has_any_error())
-
-    def test_unicode(self):
-        """
-        Проверка создания контакта с нетипичными unicode символами
-        """
-
-        self.page.create_contact(nick="ђћ∆".decode("utf-8"))
-        self.assertFalse(self.page.has_any_error())
-
-    def test_two_emails(self):
-        """
-        Проверка создания контакта с двумя email'ами
-        """
-        self.page.create_contact(**self.contact_with_many_emails)
+        for _ in range(2):
+            self.page.create_contact(**self.dmitry_contact)
 
         self.assertFalse(self.page.has_any_error())
