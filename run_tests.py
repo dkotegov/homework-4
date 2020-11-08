a02bc1236bf2a326fc826d8291f94eb639fdbde1
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-11-08 20:31:47 +0300

    run file changed

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,14 @@
 import sys
 import unittest
+
+from tests.pages.manage_orders_page import ManageOrderPage
+from tests.tests_potapchuk.feedback_tests import FeedbackTest
+from tests.tests_potapchuk.map_tests import MapTest
+from tests.tests_potapchuk.notification_tests import NotificationTest
+from tests.tests_potapchuk.order_approve_tests import OrderApproveTest
+from tests.tests_potapchuk.order_history_tests import OrderHistoryTest
+from tests.tests_potapchuk.restaurant_tests import RestaurantTest
+from tests.tests_potapchuk.support_chat_list_tests import ChatListTest
 from tests.tests_yakovidis.authentication import AuthenticationTest
 from tests.tests_yakovidis.profile import ProfileTest
 from tests.tests_yakovidis.registration import RegistrationTest
@@ -11,6 +20,9 @@ from tests.tests_postnikov.add_rest_point import AddPointTest
 from tests.tests_postnikov.add_products import AddProductTest
 from tests.tests_postnikov.manage_orders import ManageOrdersTest
 from tests.tests_postnikov.rest_tags import ManageRestaurantTagsTest
+
+from tests.tests_potapchuk.support_chat_tests import ChatTest
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
@@ -25,6 +37,17 @@ if __name__ == '__main__':
         unittest.makeSuite(AddProductTest),
         unittest.makeSuite(ManageRestaurantTagsTest),
         unittest.makeSuite(ManageOrdersTest),
+
+        # unittest.makeSuite(MapTest),
+
+        unittest.makeSuite(NotificationTest),
+        unittest.makeSuite(OrderHistoryTest),
+        unittest.makeSuite(OrderApproveTest),
+        unittest.makeSuite(RestaurantTest),
+        unittest.makeSuite(FeedbackTest),
+        unittest.makeSuite(ChatTest),
+        unittest.makeSuite(ChatListTest),
+
     ))
 
     result = unittest.TextTestRunner(verbosity=2).run(suite)
