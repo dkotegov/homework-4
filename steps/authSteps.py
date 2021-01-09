c7c10bc0186802d4972f10d23758439557b6eedf
Elderly-AI <aae_pl@mail.ru>
2021-01-09 17:13:11 +0300

    fix login

--- steps/authSteps.py
+++ steps/authSteps.py
@@ -8,16 +8,18 @@ class AuthForm(BaseSteps):
     SUBMIT_BUTTON = '//button[@data-test-id="submit-button"]'
 
     def set_login(self, login):
-        self.wait_until_and_get_elem_by_xpath(self.LOGIN).send_keys(login)
+        self.wait_to_be_clickable_by_xpath(self.LOGIN).send_keys(login)
 
     def click_next(self):
-        self.wait_until_and_get_elem_by_xpath(self.NEXT_BUTTON).click()
+        self.wait_to_be_clickable_by_xpath(self.NEXT_BUTTON).click()
 
     def set_password(self, password):
-        self.wait_until_and_get_elem_by_xpath(self.PASSWORD).send_keys(password)
+        password_input = self.wait_to_be_clickable_by_xpath(self.PASSWORD)
+        password_input.click()
+        password_input.send_keys(password)
 
     def submit(self):
-        self.wait_until_and_get_elem_by_xpath(self.SUBMIT_BUTTON).click()
+        self.wait_to_be_clickable_by_xpath(self.SUBMIT_BUTTON).click()
 
     def wait_for_cookie(self):
         self.wait_for_url("https://e.mail.ru/inbox/?afterReload=1")
