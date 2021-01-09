5f3bb3a8bae7730dad036479c5975021b52d5012
Дмитрий Гуляченков <d.gulyachenkov@corp.mail.ru>
2021-01-09 17:01:30 +0300

    fix window not closed

--- casses/base/BaseTest.py
+++ casses/base/BaseTest.py
@@ -3,23 +3,27 @@ import unittest
 
 from selenium.webdriver import DesiredCapabilities, Remote
 from pages.AuthPage import AuthPage
+from selenium.common.exceptions import TimeoutException
 
 
 class BaseTest(unittest.TestCase):
     """Базовый тестовый класс"""
 
     def setUp(self):
-        browser = os.environ.get('BROWSER', 'CHROME')
-        self.driver = Remote(
-            command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=getattr(DesiredCapabilities, browser).copy()
-        )
+        try:
+            browser = os.environ.get('BROWSER', 'CHROME')
+            self.driver = Remote(
+                command_executor='http://127.0.0.1:4444/wd/hub',
+                desired_capabilities=getattr(DesiredCapabilities, browser).copy()
+            )
 
-        self.LOGIN = os.environ['LOGIN']
-        self.PASSWORD = os.environ['PASSWORD']
+            self.LOGIN = os.environ['LOGIN']
+            self.PASSWORD = os.environ['PASSWORD']
 
-        auth_page = AuthPage(self.driver)
-        auth_page.auth(os.environ['LOGIN'], os.environ['PASSWORD'])
+            auth_page = AuthPage(self.driver)
+            auth_page.auth(os.environ['LOGIN'], os.environ['PASSWORD'])
+        except TimeoutException:
+            self.driver.quit()
 
     def tearDown(self):
         self.driver.quit()
