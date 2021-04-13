b3f173c82ecbf541adb8ba24a801adaca66436b7
ErikNabiev <er2000nabiev@mail.ru>
2021-04-13 16:43:35 +0300

    logout

--- Pages/auth_page.py
+++ Pages/auth_page.py
@@ -13,19 +13,32 @@ class AuthPage(Page):
     PASSWORD = '//input[@name="password"]'
     SUBMIT = '//button[@class="name__button--2Ten8 name__buttons__marginForFilmCard--29k8-"]'
     ICON = '//img[@class="name__round--21Oxj"]'
+    LOGOUT = '//button[text()="Выйти"]'
+    ENTRY = '//button[text()="Войти"]'
 
-    def set_login(self):
-        self.driver.find_element_by_xpath(self.LOGIN).send_keys(self.USERNAME_INPUT)
+    def set_login(self, login):
+        self.driver.find_element_by_xpath(self.LOGIN).send_keys(login)
 
-    def set_password(self):
-        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(self.PASSWORD_INPUT)
+    def set_password(self, pwd):
+        self.driver.find_element_by_xpath(self.PASSWORD).send_keys(pwd)
 
     def submit(self):
         self.driver.find_element_by_xpath(self.SUBMIT).click()
 
+    def logout(self):
+        self.driver.find_element_by_xpath(self.LOGOUT).click()
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, self.ENTRY)))
+
+
     def auth(self):
         self.open()
-        self.set_login()
-        self.set_password()
+        self.set_login(self.USERNAME_INPUT)
+        self.set_password(self.PASSWORD_INPUT)
         self.submit()
         WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, self.ICON)))
+
+    def auth_wrong(self, login, password):
+        self.open()
+        self.set_login(login)
+        self.set_password(password)
+        self.submit()
