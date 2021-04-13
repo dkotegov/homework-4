f8331be3406c46c59f4950ee1eff5c83de5502d0
ErikNabiev <er2000nabiev@mail.ru>
2021-04-13 16:10:39 +0300

    fix

--- Pages/auth_page.py
+++ Pages/auth_page.py
@@ -1,4 +1,7 @@
 from Pages.page import Page
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import os
 
 
@@ -8,7 +11,8 @@ class AuthPage(Page):
     PATH = '/login'
     LOGIN = '//input[@name="login"]'
     PASSWORD = '//input[@name="password"]'
-    SUBMIT = '//button[text()="Войти"]'
+    SUBMIT = '//button[@class="name__button--2Ten8 name__buttons__marginForFilmCard--29k8-"]'
+    ICON = '//img[@class="name__round--21Oxj"]'
 
     def set_login(self):
         self.driver.find_element_by_xpath(self.LOGIN).send_keys(self.USERNAME_INPUT)
@@ -24,3 +28,4 @@ class AuthPage(Page):
         self.set_login()
         self.set_password()
         self.submit()
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, self.ICON)))
