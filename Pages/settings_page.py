b0e4c745e06fdfc53f6ab79010960ffcb17afa79
Arkadiy <chekryzhov2000@mail.ru>
2021-04-13 17:34:47 +0300

    test_change_pass

--- Pages/settings_page.py
+++ Pages/settings_page.py
@@ -1,20 +1,37 @@
 from Pages.page import Page
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import os
 
 
 class SettingPage(Page):
+    USERNAME_INPUT = os.environ['LOGIN']
+    PASSWORD_INPUT = os.environ['PASSWORD']
     PATH = '/profileChange'
     OLD = '//input[@placeholder="Старый пароль"]'
     NEW = '//input[@placeholder="Новый пароль"]'
     REPEAT = '//input[@placeholder="Повторите новый пароль"]'
     SUBMIT = '//button[text()="Сохранить"]'
+    ERROR_WRONG_OLD = '//div[text()="\nНеправильный старый пароль"][@class="name__error--FQ9hR"]'
+    ERROR_DIFF_NEW = '//div[text()="Пароли не совпадают"][@class="name__error--FQ9hR"]'
 
     def set_old_pass(self, old):
         self.driver.find_element_by_xpath(self.OLD).send_keys(old)
 
     def set_new_pass(self, new):
         self.driver.find_element_by_xpath(self.NEW).send_keys(new)
+
+    def set_new_pass_confirm(self, new):
         self.driver.find_element_by_xpath(self.REPEAT).send_keys(new)
 
     def submit(self):
         self.driver.find_element_by_xpath(self.SUBMIT).click()
+
+    def change_pass(self, old, new, new_conf):
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, self.OLD)))
+        self.set_old_pass(old)
+        self.set_new_pass(new)
+        self.set_new_pass_confirm(new_conf)
+        self.submit()
 
--- tests/setting_test.py
+++ tests/setting_test.py
@@ -7,6 +7,9 @@ import time
 
 from Pages.auth_page import AuthPage
 from Pages.settings_page import SettingPage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class SettingsTests(unittest.TestCase):
@@ -21,4 +24,29 @@ class SettingsTests(unittest.TestCase):
         auth_page.auth()
         setting_page = SettingPage(self.driver)
         setting_page.open()
-        time.sleep(10)
+        new_pass = "new_password"
+        setting_page.change_pass(setting_page.PASSWORD_INPUT, new_pass, new_pass)
+        auth_page.logout()
+        auth_page.open()
+        auth_page.PASSWORD_INPUT = new_pass
+        auth_page.auth()
+        setting_page.open()
+        setting_page.change_pass(new_pass, setting_page.PASSWORD_INPUT, setting_page.PASSWORD_INPUT)
+
+    def test_change_password_wrong_old(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.auth()
+        setting_page = SettingPage(self.driver)
+        setting_page.open()
+        new_pass = "new_password"
+        setting_page.change_pass(setting_page.PASSWORD_INPUT+"1", new_pass, new_pass)
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, setting_page.ERROR_WRONG_OLD)))
+
+    def test_change_password_different_new(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.auth()
+        setting_page = SettingPage(self.driver)
+        setting_page.open()
+        new_pass = "new_password"
+        setting_page.change_pass(setting_page.PASSWORD_INPUT, new_pass, new_pass+"1")
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, setting_page.ERROR_DIFF_NEW)))
