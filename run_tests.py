70e6ac128485ba5ca8d92f91b6fbf2f637c69708
Amaterasy17 <sheb70017@gmail.com>
2021-04-14 18:59:22 +0300

    have done tests for adding emails

--- run_tests.py
+++ run_tests.py
@@ -4,12 +4,15 @@ from tests.folder_tests.test.folder_test import FolderTest
 from tests.contacts_and_addresses.test.phone_test import PhoneTest
 from tests.folder_tests.test.pop3_test import POP3Test
 from tests.folder_tests.test.to_edit_test import ToEditTest
+from tests.contacts_and_addresses.test.email_test import EmailTest
+
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
         # unittest.makeSuite(FolderTest),
-        unittest.makeSuite(PhoneTest),
+        # unittest.makeSuite(PhoneTest),
+        unittest.makeSuite(EmailTest),
         # unittest.makeSuite(POP3Test),
         # unittest.makeSuite(ToEditTest),
     ))
---
+++ tests/contacts_and_addresses/test/email_test.py
@@ -0,0 +1,107 @@
+import os
+import time
+import unittest
+from selenium.webdriver import DesiredCapabilities, Remote
+
+from tests.contacts_and_addresses.src.auth_page import AuthPage
+from tests.contacts_and_addresses.src.email_page import EmailPage
+
+
+class EmailTest(unittest.TestCase):
+    LOGIN = os.environ['LOGIN']
+    PASSWORD = os.environ['PASSWORD']
+
+    def setUp(self):
+        browser = os.environ.get('BROWSER', 'CHROME')
+        # browser = os.environ.get('BROWSER', 'FIREFOX')
+
+        self.driver = Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=getattr(DesiredCapabilities, browser).copy()
+        )
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_error_ru(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+        auth_form = auth_page.form
+        auth_form.authorize(self.LOGIN, self.PASSWORD)
+
+        auth_page.top_menu.get_username()
+
+        email_page = EmailPage(self.driver)
+        email_page.open()
+
+        email_form = email_page.form
+        test_email = 'ваукакойты'
+        email_form.add_email(test_email)
+        error = email_form.check_error()
+        self.assert_(error == 'Incorrect email address. Enter a different one.')
+
+    def test_success_input(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+        auth_form = auth_page.form
+        auth_form.authorize(self.LOGIN, self.PASSWORD)
+
+        auth_page.top_menu.get_username()
+
+        email_page = EmailPage(self.driver)
+        email_page.open()
+
+        email_form = email_page.form
+        test_email = 'test.michael@mail.ru'
+        email_form.add_email('test.michael@mail.ru')
+        email = email_form.get_success()
+        self.assert_(email == test_email)
+
+    def test_not_valid(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+        auth_form = auth_page.form
+        auth_form.authorize(self.LOGIN, self.PASSWORD)
+
+        auth_page.top_menu.get_username()
+
+        email_page = EmailPage(self.driver)
+        email_page.open()
+
+        email_form = email_page.form
+        test_email = 'sheb70017'
+        email_form.add_email(test_email)
+        error = email_form.check_error()
+        self.assert_(error == 'Incorrect email address. Enter a different one.')
+
+    def test_add_email_with_same_name(self):  # возможен ввод в поле резервной почты адреса, совпадающего с адресом основной почты;
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+        auth_form = auth_page.form
+        auth_form.authorize(self.LOGIN, self.PASSWORD)
+
+        auth_page.top_menu.get_username()
+
+        email_page = EmailPage(self.driver)
+        email_page.open()
+
+        email_form = email_page.form
+        test_email = self.LOGIN + '@mail.ru'
+        email_form.add_email(test_email)
+        email = email_form.get_success()
+        self.assert_(email == test_email)  # вообще я искренне удивлен, что оно так работает
+
+    def test_cancel(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+        auth_form = auth_page.form
+        auth_form.authorize(self.LOGIN, self.PASSWORD)
+
+        auth_page.top_menu.get_username()
+
+        email_page = EmailPage(self.driver)
+        email_page.open()
+
+        email_form = email_page.form
+        email_form.open_adding()
+        email_form.cancel()
