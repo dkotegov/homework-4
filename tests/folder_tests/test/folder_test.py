00d820bfaf8b98cb31df5fd7df749f4b432961dd
Andrey_Mavrin <andmavrin@yandex.ru>
2021-04-14 13:09:24 +0300

    added clear method

--- tests/folder_tests/test/folder_test.py
+++ tests/folder_tests/test/folder_test.py
@@ -23,24 +23,24 @@ class FolderTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    # def test(self):
-    #     auth_page = AuthPage(self.driver)
-    #     auth_page.open()
-    #
-    #     auth_form = auth_page.form
-    #     auth_form.authorize(self.LOGIN, self.PASSWORD)
-    #
-    #     main_page = MainPage(self.driver)
-    #     main_form = main_page.main_form
-    #
-    #     folder_name = 'Добавленная папка'
-    #     main_form.add_folder_popup()
-    #     folder_form = main_page.add_folder_form
-    #     folder_form.create_folder(folder_name)
-    #
-    #     main_form.remove_folder_popup()
-    #     folder_form_remove = main_page.remove_folder_form
-    #     folder_form_remove.remove_folder(folder_name)
+    def test(self):
+        auth_page = AuthPage(self.driver)
+        auth_page.open()
+
+        auth_form = auth_page.form
+        auth_form.authorize(self.LOGIN, self.PASSWORD)
+
+        main_page = MainPage(self.driver)
+        main_form = main_page.main_form
+
+        folder_name = 'Добавленная папка'
+        main_form.add_folder_popup()
+        folder_form = main_page.add_folder_form
+        folder_form.create_folder(folder_name)
+
+        main_form.remove_folder_popup()
+        folder_form_remove = main_page.remove_folder_form
+        folder_form_remove.remove_folder(folder_name)
 
     def test_clear(self):
         auth_page = AuthPage(self.driver)
