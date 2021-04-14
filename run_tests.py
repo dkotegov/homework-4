128e06c3fe525dbabd4d19ce1de1c377b4157f77
Amaterasy17 <sheb70017@gmail.com>
2021-04-14 17:24:03 +0300

    have done test of authorization

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,15 @@
 import sys
 import unittest
 from tests.folder_tests.test.folder_test import FolderTest
+from tests.contacts_and_addresses.test.phone_test import PhoneTest
 from tests.folder_tests.test.pop3_test import POP3Test
 from tests.folder_tests.test.to_edit_test import ToEditTest
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
-        unittest.makeSuite(FolderTest),
+        # unittest.makeSuite(FolderTest),
+        unittest.makeSuite(PhoneTest),
         # unittest.makeSuite(POP3Test),
         # unittest.makeSuite(ToEditTest),
     ))
