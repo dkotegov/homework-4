bfa7e647fc9dffb602978e71c9a0c301cd9192c7
marksadykov <sadykov.mark@gmail.com>
2021-04-15 23:43:56 +0300

    test-cases

--- run_tests.py
+++ run_tests.py
@@ -42,21 +42,58 @@ class PasswordEmptyTestCase(unittest.TestCase):
 class EmailMistakeTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox(executable_path='/home/mark/.wdm/drivers/geckodriver/linux64/v0.29.1/geckodriver')
+        self.browser = webdriver.Firefox(
+            executable_path='/home/mark/.wdm/drivers/geckodriver/linux64/v0.29.1/geckodriver')
         self.addCleanup(self.browser.quit)
         self.browser.get('https://mailer.ru.com/signin')
 
     def testEmailMistake(self):
-        email = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > section > div > div > section > div > form > fieldset > div:nth-child(1) > input:nth-child(1)')))
+        email = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                                      'body > section > div > div > section > div > form > fieldset > div:nth-child(1) > input:nth-child(1)')))
         email.send_keys('name')
-        password = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > section > div > div > section > div > form > fieldset > div:nth-child(2) > input')))
+        password = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                                         'body > section > div > div > section > div > form > fieldset > div:nth-child(2) > input')))
         password.send_keys('pass')
         enter = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
-                                                                   'body > section > div > div > section > div > form > fieldset > div.row > div.column.large-4 > button')))
+                                                                                      'body > section > div > div > section > div > form > fieldset > div.row > div.column.large-4 > button')))
         enter.click()
 
-        error = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'p#emailError')))
+        error = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'p#emailError')))
         self.assertIn('Do not find this user in db', error.get_attribute('innerText'))
+
+
+class SendLetterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path='/home/mark/.wdm/drivers/geckodriver/linux64/v0.29.1/geckodriver')
+        self.addCleanup(self.browser.quit)
+        self.browser.get('https://mailer.ru.com/signin')
+        email = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                                      'body > section > div > div > section > div > form > fieldset > div:nth-child(1) > input:nth-child(1)')))
+        email.send_keys('mark')
+        password = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                                         'body > section > div > div > section > div > form > fieldset > div:nth-child(2) > input')))
+        password.send_keys('mark')
+        enter = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                                      'body > section > div > div > section > div > form > fieldset > div.row > div.column.large-4 > button')))
+        enter.click()
+
+    def testSendLetter(self):
+        sendPage = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > header > div > nav > ul > li:nth-child(2) > a')))
+        sendPage.click()
+
+        to = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > section > div > div > div > form > div:nth-child(1) > input')))
+        to.send_keys('mark@mailer.ru.com')
+        theme = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > section > div > div > div > form > div:nth-child(2) > input')))
+        theme.send_keys('Тема')
+        message = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > section > div > div > div > form > div.message.form-field > textarea')))
+        message.send_keys('Сообщение')
+
+        button = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > section > div > div > div > form > div.row > div:nth-child(1) > button')))
+        button.click()
+        self.assertIn('Письма', self.browser.title)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
