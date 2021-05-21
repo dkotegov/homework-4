d3cd0805b07eacaaad7855207d634d582b8a5d43
Arkadiy <chekryzhov2000@mail.ru>
2021-05-21 18:20:00 +0300

    auth begin

--- run_tests.py
+++ run_tests.py
@@ -2,22 +2,16 @@
 
 import sys
 import unittest
-from tests.auth_tests import AuthTests
-from tests.setting_test import SettingsTests
 from tests.signup.signup_success import SignupSuccessTests
 from tests.signup.signup_wrong import SignupWrongTests
-from tests.subscribe_tests import SubscribeTests
-from tests.playlist_tests import PlaylistTests
-from tests.rating_tests import RatingTests
-from tests.search_tests import SearchTests
-from tests.comment_test import CommentTests
+from tests.auth.auth_success import AuthTests
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    # suite.addTest(unittest.makeSuite(AuthTests))
+    suite.addTest(unittest.makeSuite(AuthTests))
     # suite.addTest(unittest.makeSuite(SettingsTests))
-    suite.addTest(unittest.makeSuite(SignupSuccessTests))
-    suite.addTest(unittest.makeSuite(SignupWrongTests))
+    # suite.addTest(unittest.makeSuite(SignupSuccessTests))
+    # suite.addTest(unittest.makeSuite(SignupWrongTests))
     # suite.addTest(unittest.makeSuite(SubscribeTests))
     # suite.addTest(unittest.makeSuite(PlaylistTests))
     # suite.addTest(unittest.makeSuite(RatingTests))
