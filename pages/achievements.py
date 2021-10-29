c2bb8e458fd2a2ee0da36a9bec076c328d8ee880
Alekhin Sergey <seralekhin@ozon.ru>
2021-10-30 01:20:44 +0300

    update

--- pages/achievements.py
+++ pages/achievements.py
@@ -2,7 +2,7 @@ from pages.default_page import DefaultPage
 
 
 class AchievementsPage(DefaultPage):
-    PATH = "user/78/achievements"
+    PATH = "user/1/achievements"
 
     def change_path(self, path):
         self.PATH = "user/" + path + "/achievements"
--- pages/reviews.py
+++ pages/reviews.py
@@ -2,7 +2,7 @@ from pages.default_page import DefaultPage
 
 
 class ReviewsPage(DefaultPage):
-    PATH = "user/78/reviews"
+    PATH = "user/1/reviews"
 
     def change_path(self, path):
         self.PATH = "user/" + path + "/reviews"
