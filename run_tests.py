3291d0c158f6fa847c013a7825a43f057cbae2b1
Suvorov Nikita <suvnik.2020@gmail.com>
2021-11-04 12:26:15 +0300

    [Init] Tests

--- run_tests.py
+++ run_tests.py
@@ -4,14 +4,16 @@ from cases.create_new_file import CreateNewFileTests
 from cases.create_new_folder_popup import CreateNewFolderPopupTests
 from cases.hover_over_file import HoverOverFileTests
 from cases.file_dropdown_menu import FileDropDownMenuTests
+from cases.search_file import SearchFileTests
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
-        unittest.makeSuite(CreateNewFileTests),
-        unittest.makeSuite(CreateNewFolderPopupTests),
-        unittest.makeSuite(HoverOverFileTests),
-        unittest.makeSuite(FileDropDownMenuTests),
+        # unittest.makeSuite(CreateNewFileTests),
+        # unittest.makeSuite(CreateNewFolderPopupTests),
+        # unittest.makeSuite(HoverOverFileTests),
+        # unittest.makeSuite(FileDropDownMenuTests),
+        unittest.makeSuite(SearchFileTests),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
