90ef2b8c4f0d8b620e1c96341ea8258e4de89601
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-12-07 21:17:29 +0300

    tests for main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,8 +5,13 @@ from utils.helpers import wait_for_visible
 class MainPage(DefaultPage):
     MOVIE_CARD = '.slider__card'
     VIEW_ALL_LINK = '.view-all'
+    VIEW_ALL_FOR_GENRE = '#main-genre-search-button'
     GENRE_ITEM = '.genres-list__item-box'
     MOVIE_TITLE = '.title-box__title'
+    PAGE_TITLE = '.main__page-title'
+    MOVIE_RATING = '.right__movie-rating'
+    MOVIE_CARD_TITLE = '.center__movie-link'
+    MOVIE_CARD_POSTER = '.movie-card-img'
 
     def __init__(self, driver):
         super().__init__(driver, '/')
@@ -27,3 +32,35 @@ class MainPage(DefaultPage):
     def get_movie_title(self):
         wait_for_visible(self.driver, self.MOVIE_TITLE)
         return self.driver.find_element_by_css_selector(self.MOVIE_TITLE).text
+
+    def go_to_best_movies(self):
+        wait_for_visible(self.driver, self.VIEW_ALL_LINK)
+        self.driver.find_elements_by_css_selector(self.VIEW_ALL_LINK)[0].click()
+
+    def get_page_title(self):
+        wait_for_visible(self.driver, self.PAGE_TITLE)
+        return self.driver.find_elements_by_css_selector(self.PAGE_TITLE)[0].text
+
+    def select_genre(self, genre_name):
+        wait_for_visible(self.driver, self.GENRE_ITEM)
+        [item for item in self.driver.find_elements_by_css_selector(self.GENRE_ITEM) if item.text == genre_name][0]\
+            .click()
+
+    def go_to_movies_by_genre(self):
+        wait_for_visible(self.driver, self.VIEW_ALL_FOR_GENRE)
+        self.driver.find_element_by_css_selector(self.VIEW_ALL_FOR_GENRE).click()
+        wait_for_visible(self.driver, self.PAGE_TITLE)
+
+    def get_ratings(self):
+        wait_for_visible(self.driver, self.MOVIE_RATING)
+        return [item.text for item in self.driver.find_elements_by_css_selector(self.MOVIE_RATING)]
+
+    def go_to_movie_via_title(self):
+        wait_for_visible(self.driver, self.MOVIE_CARD_TITLE)
+        self.driver.find_element_by_css_selector(self.MOVIE_CARD_TITLE).click()
+        wait_for_visible(self.driver, self.MOVIE_TITLE)
+
+    def go_to_movie_via_poster(self):
+        wait_for_visible(self.driver, self.MOVIE_CARD_POSTER)
+        self.driver.find_element_by_css_selector(self.MOVIE_CARD_POSTER).click()
+        wait_for_visible(self.driver, self.MOVIE_TITLE)
--- tests/main_page.py
+++ tests/main_page.py
@@ -10,4 +10,36 @@ class MainPageTestSuite(BaseTest):
         main.go_to_first_movie_page()
         self.assertEqual(main.get_movie_title(), title)
 
+    def test_best_movies_transition(self):
+        main = MainPage(self.driver)
+        main.open()
+        main.go_to_best_movies()
+        self.assertEqual(main.get_page_title(), 'Лучшие фильмы')
 
+    def test_go_to_genre(self):
+        main = MainPage(self.driver)
+        main.open()
+        genre = 'драма'
+        main.select_genre(genre)
+        main.go_to_movies_by_genre()
+        self.assertEqual(main.get_page_title(), f'По жанрам: {genre}')
+
+    def test_sorted_by_rating(self):
+        main = MainPage(self.driver)
+        main.open()
+        main.go_to_best_movies()
+        ratings = list(map(float, main.get_ratings()))
+        self.assertEqual(ratings, sorted(ratings, reverse=True))
+
+
+    def test_movie_page_transition_via_title(self):
+        main = MainPage(self.driver)
+        main.open()
+        main.go_to_best_movies()
+        main.go_to_movie_via_title()
+
+    def test_movie_page_transition_via_poster(self):
+        main = MainPage(self.driver)
+        main.open()
+        main.go_to_best_movies()
+        main.go_to_movie_via_poster()
